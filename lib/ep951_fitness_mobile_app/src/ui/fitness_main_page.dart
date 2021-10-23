6021d66e6ca638af72a4be2b14b5a56d4fc36789
Dreamwalker <aristojeff@gmail.com>
2021-10-23 19:28:37 +0900

    Create fitness_main_page.dart

---
+++ lib/ep951_fitness_mobile_app/src/ui/fitness_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessMainPage extends StatefulWidget {
+  const FitnessMainPage({Key? key}) : super(key: key);
+
+  @override
+  _FitnessMainPageState createState() => _FitnessMainPageState();
+}
+
+class _FitnessMainPageState extends State<FitnessMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
