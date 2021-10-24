f0689dce8635ceff5bf1e5bd00ad263e859cf1c2
Dreamwalker <aristojeff@gmail.com>
2021-10-24 19:01:22 +0900

    Update fitness_main_page.dart

--- lib/ep951_fitness_mobile_app/src/ui/fitness_main_page.dart
+++ lib/ep951_fitness_mobile_app/src/ui/fitness_main_page.dart
@@ -1,6 +1,7 @@
 import 'package:coolicons/coolicons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_16th_story/ep951_fitness_mobile_app/src/data/feature_workouts.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class FitnessMainPage extends StatefulWidget {
   const FitnessMainPage({Key? key}) : super(key: key);
@@ -30,7 +31,10 @@ class _FitnessMainPageState extends State<FitnessMainPage> {
                       SizedBox(
                         height: 4,
                       ),
-                      Text("You've free trial class"),
+                      Text("You've free trial class",style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),),
                     ],
                   ),
                   const Spacer(),
@@ -40,6 +44,7 @@ class _FitnessMainPageState extends State<FitnessMainPage> {
                           const SnackBar(
                             content: Text(
                               "Clicked Menu button",
+
                             ),
                           ),
                         );
@@ -53,15 +58,17 @@ class _FitnessMainPageState extends State<FitnessMainPage> {
               child: SizedBox(
                 height: 280,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8,8,0,8),
+                  padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text("Featured Workouts",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16
-                      ),),
-                      const SizedBox(height: 16,),
+                      const Text(
+                        "Featured Workouts",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
                       Expanded(
                         child: ListView.builder(
                           scrollDirection: Axis.horizontal,
@@ -78,21 +85,40 @@ class _FitnessMainPageState extends State<FitnessMainPage> {
                                     Container(
                                       height: 180,
                                       decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          image: NetworkImage(item.thumbImg ?? ""),
-                                          fit: BoxFit.cover,
-                                        ),
-                                        borderRadius: BorderRadius.circular(12)
-                                      ),
+                                          image: DecorationImage(
+                                            image: NetworkImage(item.thumbImg ?? ""),
+                                            fit: BoxFit.cover,
+                                          ),
+                                          borderRadius: BorderRadius.circular(12)),
+                                    ),
+                                    const SizedBox(
+                                      height: 8,
                                     ),
                                     Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                       children: [
                                         Row(
                                           children: [
-                                            Text(item.title ?? "-"),
+                                            Text(
+                                              item.title ?? "-",
+                                              style: const TextStyle(fontWeight: FontWeight.bold),
+                                            ),
                                             const Spacer(),
-                                            Text(item.time ?? "-"),
+                                            Text(
+                                              item.time ?? "-",
+                                              style: const TextStyle(color: Colors.grey,
+                                              fontSize: 13),
+                                            ),
                                           ],
+                                        ),
+                                        const SizedBox(
+                                          height: 8,
+                                        ),
+                                        Text(
+                                          item.subtitle ?? "-",
+                                          style: const TextStyle(color: Colors.grey,
+                                          fontSize: 13),
                                         )
                                       ],
                                     )
@@ -108,9 +134,23 @@ class _FitnessMainPageState extends State<FitnessMainPage> {
                 ),
               ),
             ),
-            const SizedBox(
-              height: 84,
-              child: Placeholder(),
+             SizedBox(
+              height: 72,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text("Workout Progress"),
+                        Text("12 class ongoing"),
+                      ],
+                    ),
+                    Spacer(),
+                    CircularPercentIndicator(radius: 24),
+                  ],
+                ),
+              ),
             ),
             const SizedBox(
               height: 16,
