3cdf67b0ce28cd4b1d68b0ecf41be1d12b87ab6f
Dreamwalker <aristojeff@gmail.com>
2021-10-24 18:52:51 +0900

    Update feature_workouts.dart

--- lib/ep951_fitness_mobile_app/src/data/feature_workouts.dart
+++ lib/ep951_fitness_mobile_app/src/data/feature_workouts.dart
@@ -1,5 +1,29 @@
-
-
 import 'package:flutter_notebook_16th_story/ep951_fitness_mobile_app/src/model/workout.dart';
 
-List<Workout> featureWorkoutItems = [];
+List<Workout> featureWorkoutItems = [
+  Workout(
+      title: "Body Circuit",
+      subtitle: "Romano Irminyo",
+      time: "3h 45m",
+      thumbImg: "https://cdn.pixabay.com/photo/2017/04/25/20/18/woman-2260736_960_720.jpg"),
+  Workout(
+      title: "Back and Body",
+      subtitle: "Romano Irminyo",
+      time: "2h 45m",
+      thumbImg: "https://cdn.pixabay.com/photo/2017/11/18/05/02/yoga-2959226__340.jpg"),
+  Workout(
+      title: "Back and Body",
+      subtitle: "Romano Irminyo",
+      time: "2h 45m",
+      thumbImg: "https://cdn.pixabay.com/photo/2017/08/06/00/27/yoga-2587066__340.jpg"),
+  Workout(
+      title: "Back and Body",
+      subtitle: "Romano Irminyo",
+      time: "2h 45m",
+      thumbImg: "https://cdn.pixabay.com/photo/2017/11/18/05/00/woman-2959213__340.jpg"),
+  Workout(
+      title: "Back and Body",
+      subtitle: "Romano Irminyo",
+      time: "2h 45m",
+      thumbImg: "https://cdn.pixabay.com/photo/2017/11/18/05/02/yoga-2959226__340.jpg")
+];
