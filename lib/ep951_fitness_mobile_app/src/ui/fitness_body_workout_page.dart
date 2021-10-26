5db17ec3fecc2ee66427245164544e383f73f5da
Dreamwalker <aristojeff@gmail.com>
2021-10-26 19:11:24 +0900

    Update fitness_body_workout_page.dart

--- lib/ep951_fitness_mobile_app/src/ui/fitness_body_workout_page.dart
+++ lib/ep951_fitness_mobile_app/src/ui/fitness_body_workout_page.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class FitnessBodyWorkoutPage extends StatefulWidget {
+  const FitnessBodyWorkoutPage({Key? key}) : super(key: key);
+
+  @override
+  _FitnessBodyWorkoutPageState createState() => _FitnessBodyWorkoutPageState();
+}
+
+class _FitnessBodyWorkoutPageState extends State<FitnessBodyWorkoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
