9a407c9708b9ca41286a5176ee3e655f18d8e68c
Dreamwalker <aristojeff@gmail.com>
2021-10-26 19:21:00 +0900

    Update fitness_body_workout_page.dart

--- lib/ep951_fitness_mobile_app/src/ui/fitness_body_workout_page.dart
+++ lib/ep951_fitness_mobile_app/src/ui/fitness_body_workout_page.dart
@@ -11,7 +11,40 @@ class _FitnessBodyWorkoutPageState extends State<FitnessBodyWorkoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: const Text("Body Workout"),
+        foregroundColor: Colors.black,
+        elevation: 0,
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz))],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 64,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              Container(
+                height: 240,
+                decoration: BoxDecoration(color: Colors.black),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
