1260bcab1ca74c6dd6c9cf04236d324fb56481d2
Dreamwalker <aristojeff@gmail.com>
2021-10-27 18:27:18 +0900

    Update body_page_controller.dart

--- lib/ep951_fitness_mobile_app/src/controller/body_page_controller.dart
+++ lib/ep951_fitness_mobile_app/src/controller/body_page_controller.dart
@@ -2,8 +2,15 @@ import 'package:get/get.dart';
 
 class BodyPageController extends GetxController {
   int _tabIndex = 0;
+
   increment() => _tabIndex++;
+
   int get getTabIndex => _tabIndex;
 
   set tabIndex(int idx) => _tabIndex = idx;
+
+  void setTabIndex(int idx) {
+    _tabIndex = idx;
+    update();
+  }
 }
