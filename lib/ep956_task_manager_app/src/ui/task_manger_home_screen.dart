e5c5ea45aae69f20ea67d1ddbdd78f1b943c3d02
Dreamwalker <aristojeff@gmail.com>
2021-10-28 19:34:49 +0900

    Create task_manger_home_screen.dart

---
+++ lib/ep956_task_manager_app/src/ui/task_manger_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TaskManagerHomeScreen extends StatefulWidget {
+  const TaskManagerHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _TaskManagerHomeScreenState createState() => _TaskManagerHomeScreenState();
+}
+
+class _TaskManagerHomeScreenState extends State<TaskManagerHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
