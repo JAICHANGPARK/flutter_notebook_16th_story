f44feed2ed110a9c4594cce8d4a3b42f457d8542
Dreamwalker <aristojeff@gmail.com>
2021-10-28 19:39:45 +0900

    Update task_manger_home_screen.dart

--- lib/ep956_task_manager_app/src/ui/task_manger_home_screen.dart
+++ lib/ep956_task_manager_app/src/ui/task_manger_home_screen.dart
@@ -11,33 +11,36 @@ class _TaskManagerHomeScreenState extends State<TaskManagerHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(child: Container(
-            color: Colors.black,
-          )),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-
-              height: 120,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Column(
+                children: [],
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: 120,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
                   Colors.white.withOpacity(0.7),
                   Colors.white.withOpacity(0.75),
                   Colors.white.withOpacity(0.5),
                   Colors.white.withOpacity(0.25),
                   Colors.white.withOpacity(0.1),
-
-                ], begin: Alignment.bottomCenter,
-                end: Alignment.topCenter)
+                ], begin: Alignment.bottomCenter, end: Alignment.topCenter)),
               ),
-            ),
-
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
