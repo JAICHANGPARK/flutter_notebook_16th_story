89783b0ddab902fc56c32f7cae4a1421dc2ebbe0
Dreamwalker <aristojeff@gmail.com>
2021-10-28 19:34:46 +0900

    Update task_manager_app.dart

--- lib/ep956_task_manager_app/task_manager_app.dart
+++ lib/ep956_task_manager_app/task_manager_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_16th_story/ep956_task_manager_app/src/ui/task_manger_home_screen.dart';
 import 'package:get/get.dart';
 
 
@@ -7,8 +8,8 @@ class TaskManagerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-
+    return const GetMaterialApp(
+        home: TaskManagerHomeScreen(),
     );
   }
 }
