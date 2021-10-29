4a25ab21bbe1d73a020dd08c2d2353e89c4803c9
Dreamwalker <aristojeff@gmail.com>
2021-10-29 20:36:16 +0900

    Update today_task.dart

--- lib/ep956_task_manager_app/src/model/today_task.dart
+++ lib/ep956_task_manager_app/src/model/today_task.dart
@@ -1,20 +1,26 @@
 
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import 'color_converter.dart';
 part 'today_task.freezed.dart';
 part 'today_task.g.dart';
 
 @freezed
 class TodayTask with _$TodayTask {
-  factory TodayTask({
+  const factory TodayTask({
     List<String>? tags,
     String? title,
     String? date,
     String? time,
     bool? isCompleted,
     String? remindAt,
+    @ColorIntConv()
+    @Default(ColorIntConv.defaultColor)
+    @JsonKey()
+    Color backgroundColorRGB,
   }) = _TodayTask;
 
-  factory TodayTask.fromJson(Map<String, dynamic> json) => _$TodayTaskFromJson(json);
-
+  factory TodayTask.fromJson(Map<String, dynamic> json) =>
+      _$TodayTaskFromJson(json);
 }
