c9c432f11ac0a179a26aa196e301e33cf4e04158
Dreamwalker <aristojeff@gmail.com>
2021-10-29 20:37:48 +0900

    Update fake_today_task.dart

--- lib/ep956_task_manager_app/src/data/fake_today_task.dart
+++ lib/ep956_task_manager_app/src/data/fake_today_task.dart
@@ -1,54 +1,53 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_notebook_16th_story/ep956_task_manager_app/src/model/today_task.dart';
 
 List<TodayTask> fakeTodayTask = [
-  TodayTask(
+  const TodayTask(
     time: "7:30",
     title: "Taking My Sister To School",
     date: "12 October 2021",
     isCompleted: false,
     tags: ["School", "Everyday"],
-    remindAt: "07:15"
+    remindAt: "07:15",
+    backgroundColorRGB: Colors.blue,
   ),
-  TodayTask(
+  const TodayTask(
+    time: "7:30",
+    title: "Taking My Sister To School",
+    date: "12 October 2021",
+    isCompleted: false,
+    tags: ["School", "Everyday"],
+    remindAt: "07:15",
+    backgroundColorRGB: Colors.red,
+  ),
+  const TodayTask(
+    time: "7:30",
+    title: "Taking My Sister To School",
+    date: "12 October 2021",
+    isCompleted: false,
+    tags: ["School", "Everyday"],
+    remindAt: "07:15",
+    backgroundColorRGB: Colors.green,
+  ),
+  const TodayTask(
       time: "7:30",
       title: "Taking My Sister To School",
       date: "12 October 2021",
       isCompleted: false,
       tags: ["School", "Everyday"],
-      remindAt: "07:15"
-  ),
-  TodayTask(
+      remindAt: "07:15"),
+  const TodayTask(
       time: "7:30",
       title: "Taking My Sister To School",
       date: "12 October 2021",
       isCompleted: false,
       tags: ["School", "Everyday"],
-      remindAt: "07:15"
-  ),
-  TodayTask(
+      remindAt: "07:15"),
+  const TodayTask(
       time: "7:30",
       title: "Taking My Sister To School",
       date: "12 October 2021",
       isCompleted: false,
       tags: ["School", "Everyday"],
-      remindAt: "07:15"
-  ),
-  TodayTask(
-      time: "7:30",
-      title: "Taking My Sister To School",
-      date: "12 October 2021",
-      isCompleted: false,
-      tags: ["School", "Everyday"],
-      remindAt: "07:15"
-  ),
-  TodayTask(
-      time: "7:30",
-      title: "Taking My Sister To School",
-      date: "12 October 2021",
-      isCompleted: false,
-      tags: ["School", "Everyday"],
-      remindAt: "07:15"
-  )
-
+      remindAt: "07:15")
 ];
