0b1842db07678c1474ea09425e2d117f34ba2ae5
Dreamwalker <aristojeff@gmail.com>
2021-10-29 20:01:36 +0900

    Create fake_today_task.dart

---
+++ lib/ep956_task_manager_app/src/data/fake_today_task.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter_notebook_16th_story/ep956_task_manager_app/src/model/today_task.dart';
+
+List<TodayTask> fakeTodayTask = [
+  TodayTask(
+    time: "7:30",
+    title: "Taking My Sister To School",
+    date: "12 October 2021",
+    isCompleted: false,
+    tags: ["School", "Everyday"],
+  )
+];
