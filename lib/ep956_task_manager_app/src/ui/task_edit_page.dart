7e670d6056cdcc967a27d85431917ddf9c63a5e9
Dreamwalker <aristojeff@gmail.com>
2021-10-31 20:42:13 +0900

    Create task_edit_page.dart

---
+++ lib/ep956_task_manager_app/src/ui/task_edit_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TaskEditPage extends StatefulWidget {
+  const TaskEditPage({Key? key}) : super(key: key);
+
+  @override
+  _TaskEditPageState createState() => _TaskEditPageState();
+}
+
+class _TaskEditPageState extends State<TaskEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
