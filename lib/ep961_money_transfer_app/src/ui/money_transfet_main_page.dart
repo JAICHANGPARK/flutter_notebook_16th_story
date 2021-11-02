826c0c16466be10f589e0a50f52a7a386d5b611a
Dreamwalker <aristojeff@gmail.com>
2021-11-02 19:20:10 +0900

    Update money_transfet_main_page.dart

--- lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
+++ lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
@@ -11,9 +11,30 @@ class _MoneyTransferMainPageState extends State<MoneyTransferMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: [
-
+          Row(
+            children: [
+              Icon(
+                Icons.keyboard_arrow_left,
+                color: Colors.white,
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("Add New"),
+                      Icon(
+                        Icons.add_circle_outline,
+                        color: Colors.white,
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
