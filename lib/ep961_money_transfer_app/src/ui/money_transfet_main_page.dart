5d717c1c2c939110768df772cb1ad87f27ab9234
Dreamwalker <aristojeff@gmail.com>
2021-11-02 19:28:14 +0900

    Update money_transfet_main_page.dart

--- lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
+++ lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
@@ -15,88 +15,118 @@ class _MoneyTransferMainPageState extends State<MoneyTransferMainPage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  const Icon(
-                    Icons.keyboard_arrow_left,
-                    color: Colors.white,
-                    size: 24,
-                  ),
-                  const Spacer(),
-                  Column(
-                    children: [
-                      Row(
-                        children: const [
-                          Text(
-                            "Add New",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w300,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.keyboard_arrow_left,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                    const Spacer(),
+                    Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              "Add New",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Icon(
-                            Icons.add_circle_outline,
-                            color: Colors.white,
-                            size: 20,
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Container(
-                        height: 0.8,
-                        width: 74,
-                        color: Colors.white,
-                      )
-                    ],
-                  )
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  "My Cards",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w800,
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Icon(
+                              Icons.add_circle_outline,
+                              color: Colors.white,
+                              size: 20,
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Container(
+                          height: 0.8,
+                          width: 74,
+                          color: Colors.white,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    "My Cards",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.pink[50],
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Container(
-                  height: 140,
+                Container(
+                  height: 200,
                   decoration: BoxDecoration(
-                    color: Colors.grey[50],
+                    color: Colors.pink[50],
                     borderRadius: BorderRadius.circular(16),
                   ),
                 ),
-              ),
-              const Text(
-                "Today",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w800,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Container(
+                    height: 140,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                const Text(
+                  "Today",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Container(
+                    height: 140,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+                const Text(
+                  "Yesterday",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
