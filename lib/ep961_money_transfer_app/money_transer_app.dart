3afaa60579a24ba285f0d21f8716f35bff2920bc
Dreamwalker <aristojeff@gmail.com>
2021-11-02 19:17:12 +0900

    Create money_transer_app.dart

---
+++ lib/ep961_money_transfer_app/money_transer_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class MoneyTransferApp extends StatelessWidget {
+  const MoneyTransferApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
