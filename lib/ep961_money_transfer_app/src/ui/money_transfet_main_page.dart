40db257fb6c4a8987490383f25a155bf59587905
Dreamwalker <aristojeff@gmail.com>
2021-11-02 19:18:06 +0900

    Create money_transfet_main_page.dart

---
+++ lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MoneyTransferMainPage extends StatefulWidget {
+  const MoneyTransferMainPage({Key? key}) : super(key: key);
+
+  @override
+  _MoneyTransferMainPageState createState() => _MoneyTransferMainPageState();
+}
+
+class _MoneyTransferMainPageState extends State<MoneyTransferMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
