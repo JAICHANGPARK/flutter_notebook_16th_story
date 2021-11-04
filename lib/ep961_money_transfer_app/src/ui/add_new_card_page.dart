f35c26c865efd17687f1018e8387e071487f15ad
Dreamwalker <aristojeff@gmail.com>
2021-11-04 19:35:35 +0900

    Create add_new_card_page.dart

---
+++ lib/ep961_money_transfer_app/src/ui/add_new_card_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AddNewCardPage extends StatefulWidget {
+  const AddNewCardPage({Key? key}) : super(key: key);
+
+  @override
+  _AddNewCardPage1State createState() => _AddNewCardPage1State();
+}
+
+class _AddNewCardPage1State extends State<AddNewCardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
