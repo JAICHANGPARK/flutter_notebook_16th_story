80851b4e70639b5ff4bb563ccaba03df0896d50d
Dreamwalker <aristojeff@gmail.com>
2021-11-04 19:28:29 +0900

    Update money_transfet_main_page.dart

--- lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
+++ lib/ep961_money_transfer_app/src/ui/money_transfet_main_page.dart
@@ -255,43 +255,43 @@ class _MoneyTransferMainPageState extends State<MoneyTransferMainPage> {
                   padding: const EdgeInsets.symmetric(vertical: 16),
                   child: Column(
                     children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 48,
-                              width: 48,
-                              decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(4)),
+                      Row(
+                        children: [
+                          Container(
+                            height: 42,
+                            width: 42,
+                            decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(4)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "You received a payment",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 4,
+                                ),
+                                Text(
+                                  "Dreamwalker",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "You received a payment",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 4,
-                                  ),
-                                  Text(
-                                    "Dreamwalker",
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Spacer(),
-                            Text("\$220.00")
-                          ],
-                        ),
+                          ),
+                          Spacer(),
+                          Text("\$220.00", style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                          ),)
+                        ],
                       ),
                     ],
                   ),
