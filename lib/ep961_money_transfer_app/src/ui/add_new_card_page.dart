534b5c9d9d49b702add75925fd8848d55395168e
Dreamwalker <aristojeff@gmail.com>
2021-11-06 19:06:41 +0900

    Update add_new_card_page.dart

--- lib/ep961_money_transfer_app/src/ui/add_new_card_page.dart
+++ lib/ep961_money_transfer_app/src/ui/add_new_card_page.dart
@@ -224,6 +224,7 @@ class _AddNewCardPage1State extends State<AddNewCardPage> {
                 height: 32,
               ),
               Container(
+                padding: EdgeInsets.symmetric(vertical: 20),
                 decoration: BoxDecoration(
                   color: Colors.red[50],
                   borderRadius: BorderRadius.circular(8),
