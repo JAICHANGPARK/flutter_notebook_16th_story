72a83422de6ec0742967dbaefb191722da755b73
Dreamwalker <aristojeff@gmail.com>
2021-11-07 18:36:38 +0900

    Create bank_wallet_static_page.dart

---
+++ lib/ep965_bank_wallet_app/src/ui/bank_wallet_static_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BankWalletStaticPage extends StatefulWidget {
+  const BankWalletStaticPage({Key? key}) : super(key: key);
+
+  @override
+  _BankWalletStaticPageState createState() => _BankWalletStaticPageState();
+}
+
+class _BankWalletStaticPageState extends State<BankWalletStaticPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
