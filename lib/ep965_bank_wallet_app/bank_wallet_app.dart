feef8609d3ad7912ec15ca6d7ba72ca9e25447a2
Dreamwalker <aristojeff@gmail.com>
2021-11-07 18:37:47 +0900

    Update bank_wallet_app.dart

--- lib/ep965_bank_wallet_app/bank_wallet_app.dart
+++ lib/ep965_bank_wallet_app/bank_wallet_app.dart
@@ -2,16 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'src/ui/bank_wallet_static_page.dart';
 
-
 class BankWalletApp extends StatelessWidget {
   const BankWalletApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return const MaterialApp(
       home: BankWalletStaticPage(),
-
     );
   }
 }
