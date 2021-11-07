ec410a6497e4a94f985b63520a3472aa4949197a
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:21:06 +0900

    Update bloc_repo.dart

--- lib/state_management/bloc/repo/bloc_repo.dart
+++ lib/state_management/bloc/repo/bloc_repo.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_notebook_16th_story/state_management/bloc/model/bloc_todo.dart';
+
 class TodoRepository {
   Future<List<Map<String, dynamic>>> listTodo() async {
     await Future.delayed(const Duration(seconds: 1));
@@ -5,5 +7,15 @@ class TodoRepository {
       {'id': 1, 'title': "Future 1", 'createdAt': DateTime.now().toString()},
       {'id': 2, 'title': "Future 2", 'createdAt': DateTime.now().toString()}
     ];
+  }
+
+  Future<Map<String, dynamic>> createTodo(Todo todo) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return todo.toJson();
+  }
+
+  Future<int> deleteTodo(int id) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return id;
   }
 }
