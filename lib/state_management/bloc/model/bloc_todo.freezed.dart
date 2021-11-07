80b1c45fe7774c9e8653d0e90de78b07456a1e0d
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:19:41 +0900

    Create bloc_todo.freezed.dart

---
+++ lib/state_management/bloc/model/bloc_todo.freezed.dart
@@ -0,0 +1,191 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'bloc_todo.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Todo _$TodoFromJson(Map<String, dynamic> json) {
+  return _Todo.fromJson(json);
+}
+
+/// @nodoc
+class _$TodoTearOff {
+  const _$TodoTearOff();
+
+  _Todo call(
+      {required int id, required String title, required String createdAt}) {
+    return _Todo(
+      id: id,
+      title: title,
+      createdAt: createdAt,
+    );
+  }
+
+  Todo fromJson(Map<String, Object?> json) {
+    return Todo.fromJson(json);
+  }
+}
+
+/// @nodoc
+const $Todo = _$TodoTearOff();
+
+/// @nodoc
+mixin _$Todo {
+  int get id => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get createdAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $TodoCopyWith<Todo> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TodoCopyWith<$Res> {
+  factory $TodoCopyWith(Todo value, $Res Function(Todo) then) =
+      _$TodoCopyWithImpl<$Res>;
+  $Res call({int id, String title, String createdAt});
+}
+
+/// @nodoc
+class _$TodoCopyWithImpl<$Res> implements $TodoCopyWith<$Res> {
+  _$TodoCopyWithImpl(this._value, this._then);
+
+  final Todo _value;
+  // ignore: unused_field
+  final $Res Function(Todo) _then;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? title = freezed,
+    Object? createdAt = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: createdAt == freezed
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$TodoCopyWith<$Res> implements $TodoCopyWith<$Res> {
+  factory _$TodoCopyWith(_Todo value, $Res Function(_Todo) then) =
+      __$TodoCopyWithImpl<$Res>;
+  @override
+  $Res call({int id, String title, String createdAt});
+}
+
+/// @nodoc
+class __$TodoCopyWithImpl<$Res> extends _$TodoCopyWithImpl<$Res>
+    implements _$TodoCopyWith<$Res> {
+  __$TodoCopyWithImpl(_Todo _value, $Res Function(_Todo) _then)
+      : super(_value, (v) => _then(v as _Todo));
+
+  @override
+  _Todo get _value => super._value as _Todo;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? title = freezed,
+    Object? createdAt = freezed,
+  }) {
+    return _then(_Todo(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      createdAt: createdAt == freezed
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Todo implements _Todo {
+  _$_Todo({required this.id, required this.title, required this.createdAt});
+
+  factory _$_Todo.fromJson(Map<String, dynamic> json) => _$$_TodoFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final String title;
+  @override
+  final String createdAt;
+
+  @override
+  String toString() {
+    return 'Todo(id: $id, title: $title, createdAt: $createdAt)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _Todo &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, id, title, createdAt);
+
+  @JsonKey(ignore: true)
+  @override
+  _$TodoCopyWith<_Todo> get copyWith =>
+      __$TodoCopyWithImpl<_Todo>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_TodoToJson(this);
+  }
+}
+
+abstract class _Todo implements Todo {
+  factory _Todo(
+      {required int id,
+      required String title,
+      required String createdAt}) = _$_Todo;
+
+  factory _Todo.fromJson(Map<String, dynamic> json) = _$_Todo.fromJson;
+
+  @override
+  int get id;
+  @override
+  String get title;
+  @override
+  String get createdAt;
+  @override
+  @JsonKey(ignore: true)
+  _$TodoCopyWith<_Todo> get copyWith => throw _privateConstructorUsedError;
+}
