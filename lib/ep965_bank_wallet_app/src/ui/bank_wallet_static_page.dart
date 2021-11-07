2d9375fd4d282dc5e5668eee68706ffdb12e8e6f
Dreamwalker <aristojeff@gmail.com>
2021-11-07 18:47:16 +0900

    Update bank_wallet_static_page.dart

--- lib/ep965_bank_wallet_app/src/ui/bank_wallet_static_page.dart
+++ lib/ep965_bank_wallet_app/src/ui/bank_wallet_static_page.dart
@@ -1,4 +1,12 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class BankTabState extends Equatable {}
+
+class BankTabCubit extends Cubit<BankTabState> {
+  BankTabCubit(BankTabState initialState) : super(initialState);
+}
 
 class BankWalletStaticPage extends StatefulWidget {
   const BankWalletStaticPage({Key? key}) : super(key: key);
@@ -8,18 +16,70 @@ class BankWalletStaticPage extends StatefulWidget {
 }
 
 class _BankWalletStaticPageState extends State<BankWalletStaticPage> {
+  int bottomNavIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: IndexedStack(
+        index: bottomNavIndex,
+        children: [
+          Container(
+            child: Text("page 1"),
+          ),
+          Container(
+            child: Text("page 2"),
+          ),
+          Container(
+            child: Text("page 3"),
+          ),
+          Container(
+            child: Text("page 4"),
+          ),
+          Container(
+            child: Text("page 5"),
+          )
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.account_balance_wallet)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.pie_chart)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.description)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    bottomNavIndex = 0;
+                  });
+                },
+                icon: Icon(Icons.home)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    bottomNavIndex = 1;
+                  });
+                },
+                icon: Icon(Icons.account_balance_wallet)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    bottomNavIndex = 2;
+                  });
+                },
+                icon: Icon(Icons.pie_chart)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    bottomNavIndex = 3;
+                  });
+                },
+                icon: Icon(Icons.description)),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    bottomNavIndex = 4;
+                  });
+                },
+                icon: Icon(Icons.settings)),
           ],
         ),
       ),
