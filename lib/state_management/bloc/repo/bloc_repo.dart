222f7abc25751a48d1ccae182360101ca629e40d
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:19:44 +0900

    Update bloc_repo.dart

--- lib/state_management/bloc/repo/bloc_repo.dart
+++ lib/state_management/bloc/repo/bloc_repo.dart
@@ -1,4 +1,9 @@
-
-class TodoRepository{
-  
+class TodoRepository {
+  Future<List<Map<String, dynamic>>> listTodo() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return [
+      {'id': 1, 'title': "Future 1", 'createdAt': DateTime.now().toString()},
+      {'id': 2, 'title': "Future 2", 'createdAt': DateTime.now().toString()}
+    ];
+  }
 }
