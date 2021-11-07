68d69bc3bca3db4d836207dbdc85d45e48633829
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:36:08 +0900

    Update todo_bloc.dart

--- lib/state_management/bloc/todo_bloc.dart
+++ lib/state_management/bloc/todo_bloc.dart
@@ -1,8 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_notebook_16th_story/state_management/bloc/repo/bloc_repo.dart';
 import 'package:flutter_notebook_16th_story/state_management/bloc/todo_event.dart';
 import 'package:flutter_notebook_16th_story/state_management/bloc/todo_state.dart';
 
-class TodoBloc extends Bloc<TodoEvent, TodoState>{
-  TodoBloc(TodoState initialState) : super(initialState);
-  
+class TodoBloc extends Bloc<TodoEvent, TodoState> {
+  final TodoRepository repository;
+
+  TodoBloc({
+    required this.repository,
+  }) : super(Empty());
+
+  @override
+  Stream<TodoState> mapEventToState(TodoEvent event) {
+    // TODO: implement mapEventToState
+    return super.mapEventToState(event);
+  }
 }
