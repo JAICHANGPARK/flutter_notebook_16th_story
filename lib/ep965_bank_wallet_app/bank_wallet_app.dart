450d3791b2987bd687acd84f45b23511f6b7f12c
Dreamwalker <aristojeff@gmail.com>
2021-11-07 18:34:23 +0900

    Create bank_wallet_app.dart

---
+++ lib/ep965_bank_wallet_app/bank_wallet_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class BankWalletApp extends StatelessWidget {
+  const BankWalletApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      
+    );
+  }
+}
