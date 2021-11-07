cb1d008bb056bb245cc31fc99d1f629b1bff4d0e
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:19:43 +0900

    Create bloc_todo.g.dart

---
+++ lib/state_management/bloc/model/bloc_todo.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bloc_todo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Todo _$$_TodoFromJson(Map<String, dynamic> json) => _$_Todo(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      createdAt: json['createdAt'] as String,
+    );
+
+Map<String, dynamic> _$$_TodoToJson(_$_Todo instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'createdAt': instance.createdAt,
+    };
