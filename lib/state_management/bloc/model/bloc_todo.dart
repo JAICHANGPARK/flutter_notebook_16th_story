ce9dc19ca41a06ed0b8abbb0ea05f0598a18f59a
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:19:38 +0900

    Update bloc_todo.dart

--- lib/state_management/bloc/model/bloc_todo.dart
+++ lib/state_management/bloc/model/bloc_todo.dart
@@ -1,8 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'todo.freezed.dart';
+part 'bloc_todo.freezed.dart';
+part 'bloc_todo.g.dart';
 
 @freezed
 class Todo with _$Todo {
   factory Todo({required int id, required String title, required String createdAt}) = _Todo;
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
 }
