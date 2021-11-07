5b907e20e657573e01b162f8d2e7a566354d5751
Dreamwalker <aristojeff@gmail.com>
2021-11-07 13:45:52 +0900

    Update todo_event.dart

--- lib/state_management/bloc/todo_event.dart
+++ lib/state_management/bloc/todo_event.dart
@@ -10,12 +10,12 @@ class ListTodosEvent extends TodoEvent {
 }
 
 class CreateTodoEvent extends TodoEvent {
-  final Todo todo;
+  final String title;
 
-  CreateTodoEvent({required this.todo});
+  CreateTodoEvent({required this.title});
 
   @override
-  List<Object?> get props => [todo];
+  List<Object?> get props => [title];
 }
 
 class DeleteTodoEvent extends TodoEvent {
