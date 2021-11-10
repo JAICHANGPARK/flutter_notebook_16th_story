6f220a7a308b89f7507bcb027cc0689a6aad51e2
Dreamwalker <aristojeff@gmail.com>
2021-11-10 21:35:12 +0900

    Create yoga_home_screen.dart

---
+++ lib/ep968_yoga_app/src/ui/yoga_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class YogaHomeScreen extends StatefulWidget {
+  const YogaHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _YogaHomeScreenState createState() => _YogaHomeScreenState();
+}
+
+class _YogaHomeScreenState extends State<YogaHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
