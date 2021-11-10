0755a30a0f02bf0f4bcdd7acca96b56cd1043b75
Dreamwalker <aristojeff@gmail.com>
2021-11-10 21:39:23 +0900

    Update yoga_home_screen.dart

--- lib/ep968_yoga_app/src/ui/yoga_home_screen.dart
+++ lib/ep968_yoga_app/src/ui/yoga_home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class YogaHomeScreen extends StatefulWidget {
   const YogaHomeScreen({Key? key}) : super(key: key);
 
@@ -12,30 +11,34 @@ class _YogaHomeScreenState extends State<YogaHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: 64,
-                  width: 64,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.grey[300]!),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 54,
+                    width: 54,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.grey[300]!),
+                    ),
                   ),
-                )
-              ],
-            )
-          ],
+                  Text("Classed"),
+                  CircleAvatar(
+                    radius: 28,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 84,
-          decoration: BoxDecoration(
-            color: Colors.black
-          ),
+          decoration: BoxDecoration(color: Colors.black),
         ),
       ),
     );
