a4cd5db83bec08e54b1c03275a20a3bc90d876ad
Dreamwalker <aristojeff@gmail.com>
2021-11-10 21:35:14 +0900

    Create yoga_app.dart

---
+++ lib/ep968_yoga_app/src/yoga_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class YogaApp extends StatelessWidget {
+  const YogaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: YogaHomeScreen(),
+    );
+  }
+}
