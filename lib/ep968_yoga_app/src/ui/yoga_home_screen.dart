a8af400e0f3f8d8e4c5b36a4eb7aed5159650cc7
Dreamwalker <aristojeff@gmail.com>
2021-11-11 19:39:06 +0900

    Update yoga_home_screen.dart

--- lib/ep968_yoga_app/src/ui/yoga_home_screen.dart
+++ lib/ep968_yoga_app/src/ui/yoga_home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_notebook_16th_story/ep968_yoga_app/src/bloc/yoga_tab_bloc.dart';
 import 'package:ionicons/ionicons.dart';
 
+import 'yoga_bottom_bar.dart';
+
 class YogaHomeScreen extends StatefulWidget {
   const YogaHomeScreen({Key? key}) : super(key: key);
 
@@ -72,148 +74,26 @@ class _YogaHomeScreenState extends State<YogaHomeScreen> {
                 ),
                 Container(
                   child: const Center(
-                    child: const Text("Hello 1"),
+                    child: Text("Hello 1"),
                   ),
                 ),
                 Container(
                   child: const Center(
-                    child: const Text("Hello 2"),
+                    child: Text("Hello 2"),
                   ),
                 ),
-                Container(
-                  child: const Center(
-                    child: const Text("Hello 3"),
-                  ),
+                const Center(
+                  child: Text("Hello 3"),
                 ),
-                Container(
-                  child: const Center(
-                    child: const Text("Hello 4"),
-                  ),
+                const Center(
+                  child: Text("Hello 4"),
                 )
               ],
             );
           },
         ),
       ),
-      bottomNavigationBar: BlocBuilder<YogaBottomTabBlock, int>(builder: (context, index) {
-        return BottomAppBar(
-          child: Container(
-            height: 80,
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            decoration: const BoxDecoration(color: Colors.black),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    context.read<YogaBottomTabBlock>().setTabIndex(0);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Ionicons.home,
-                        color: index == 0 ? Colors.teal : Colors.white,
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      CircleAvatar(
-                        radius: 4,
-                        backgroundColor: index == 0 ? Colors.teal : Colors.black,
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    context.read<YogaBottomTabBlock>().setTabIndex(1);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Ionicons.apps,
-                        color: index == 1 ? Colors.teal : Colors.white,
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      CircleAvatar(
-                        radius: 4,
-                        backgroundColor: index == 1 ? Colors.teal : Colors.black,
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    context.read<YogaBottomTabBlock>().setTabIndex(2);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Ionicons.calendar,
-                        color: index == 2 ? Colors.teal : Colors.white,
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      CircleAvatar(
-                        radius: 4,
-                        backgroundColor: index == 2 ? Colors.teal : Colors.black,
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    context.read<YogaBottomTabBlock>().setTabIndex(3);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Ionicons.bar_chart,
-                        color: index == 3 ? Colors.teal : Colors.white,
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      CircleAvatar(
-                        radius: 4,
-                        backgroundColor: index == 3 ? Colors.teal : Colors.black,
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    context.read<YogaBottomTabBlock>().setTabIndex(4);
-                  },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Ionicons.bookmark,
-                        color: index == 4 ? Colors.teal : Colors.white,
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      CircleAvatar(
-                        radius: 4,
-                        backgroundColor: index == 4 ? Colors.teal : Colors.black,
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      }),
+      bottomNavigationBar: const YogaBottomBar(),
     );
   }
 }
