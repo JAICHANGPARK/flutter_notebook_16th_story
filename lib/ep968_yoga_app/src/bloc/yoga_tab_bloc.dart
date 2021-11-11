44489d684b52c9a9f32d6e6a89c15263eb440e25
Dreamwalker <aristojeff@gmail.com>
2021-11-11 19:24:11 +0900

    Update yoga_tab_bloc.dart

--- lib/ep968_yoga_app/src/bloc/yoga_tab_bloc.dart
+++ lib/ep968_yoga_app/src/bloc/yoga_tab_bloc.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class YogaBottomTabBlock extends Cubit<int> {
-  
+  YogaBottomTabBlock() : super(0);
+
+  setTabIndex(int idx) {
+    emit(idx);
+  }
 }
