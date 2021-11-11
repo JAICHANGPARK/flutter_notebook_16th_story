450e1f03c29751fe29159d83f8c6558b11ffffae
Dreamwalker <aristojeff@gmail.com>
2021-11-11 19:38:07 +0900

    Create yoga_bottom_bar.dart

---
+++ lib/ep968_yoga_app/src/ui/yoga_bottom_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class YogaBottomBar extends StatelessWidget {
+  const YogaBottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
