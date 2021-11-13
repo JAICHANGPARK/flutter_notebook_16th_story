7b644de2c6568a1e1ff2a5346eff209c63d3fbce
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:30:20 +0900

    Update farmacia_home_page.dart

--- lib/ep971_farmacia_app/ui/farmacia_home_page.dart
+++ lib/ep971_farmacia_app/ui/farmacia_home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_16th_story/ep971_farmacia_app/controller/farmacia_home_controller.dart';
+import 'package:get/get.dart';
 
 class FarmaciaHomePage extends StatelessWidget {
   const FarmaciaHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final FarmaciaScreenController farmaciaScreenController = Get.put(FarmaciaScreenController());
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
         child: Container(
