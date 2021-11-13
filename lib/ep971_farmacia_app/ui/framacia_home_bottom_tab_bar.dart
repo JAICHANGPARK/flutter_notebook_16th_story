978fba0c7bda8ed81fcb54545008d4d4ae75b369
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:37:53 +0900

    Create framacia_home_bottom_tab_bar.dart

---
+++ lib/ep971_farmacia_app/ui/framacia_home_bottom_tab_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class FarmaciaHomeBottomTabBar extends StatelessWidget {
+  const FarmaciaHomeBottomTabBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
