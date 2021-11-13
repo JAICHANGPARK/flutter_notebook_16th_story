445115e915a0013d862dfe00dff5514202476e38
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:30:18 +0900

    Update farmacia_home_controller.dart

--- lib/ep971_farmacia_app/controller/farmacia_home_controller.dart
+++ lib/ep971_farmacia_app/controller/farmacia_home_controller.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
 
+class FarmaciaScreenController extends GetxController {
+  int bottomTabIndex = 0;
 
-class FarmaciaScreenController extends GetxController{
-  
-
+  setBottomTabIndex(int idx) {
+    bottomTabIndex = idx;
+    update();
+  }
 }
