87e77174d4806bb43a18d90308bfb3a224dcb2e4
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:25:23 +0900

    Update farmacia_home_page.dart

--- lib/ep971_farmacia_app/ui/farmacia_home_page.dart
+++ lib/ep971_farmacia_app/ui/farmacia_home_page.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
-
-
 class FarmaciaHomePage extends StatelessWidget {
   const FarmaciaHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 72,
+          child: Row(
+            children: [
+              Flexible(
+                  child: Container(
+                    width: 100,
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(color: Colors.black),
+                child: Row(
+                  children: [Icon(Icons.apps), Text("Home")],
+                ),
+              )),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.shopping_cart,
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
