07bccdd2d169c2368a82839981b2873a9b186ab3
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:27:26 +0900

    Update farmacia_home_page.dart

--- lib/ep971_farmacia_app/ui/farmacia_home_page.dart
+++ lib/ep971_farmacia_app/ui/farmacia_home_page.dart
@@ -9,23 +9,44 @@ class FarmaciaHomePage extends StatelessWidget {
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 72,
-          child: Row(
-            children: [
-              Flexible(
-                  child: Container(
-                    width: 100,
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                decoration: BoxDecoration(color: Colors.black),
-                child: Row(
-                  children: [Icon(Icons.apps), Text("Home")],
-                ),
-              )),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.shopping_cart,
-                  ))
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                    child: Container(
+                      width: 100,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  decoration: BoxDecoration(color: Colors.black,
+                  borderRadius: BorderRadius.circular(4)),
+                  child: Row(
+                    children: [Icon(Icons.apps), Text("Home")],
+                  ),
+                )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.shopping_cart_outlined,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.search,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.bookmark_border,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.account_circle_outlined,
+                    )),
+
+              ],
+            ),
           ),
         ),
       ),
