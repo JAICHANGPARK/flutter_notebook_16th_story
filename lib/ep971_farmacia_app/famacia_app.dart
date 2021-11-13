694e65eaaa10e8377685f932bbe2d144006e6b2a
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:22:17 +0900

    Update famacia_app.dart

--- lib/ep971_farmacia_app/famacia_app.dart
+++ lib/ep971_farmacia_app/famacia_app.dart
@@ -3,8 +3,8 @@ import 'package:get/get.dart';
 
 import 'ui/farmacia_home_page.dart';
 
-class FamaciaApp extends StatelessWidget {
-  const FamaciaApp({Key? key}) : super(key: key);
+class FarmaciaApp extends StatelessWidget {
+  const FarmaciaApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
