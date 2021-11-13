1fe42fdd7885282e79dc95b73eb6913a8a9a5a98
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:19:11 +0900

    Update famacia_app.dart

---
+++ lib/ep971_farmacia_app/famacia_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class FamaciaApp extends StatelessWidget {
+  const FamaciaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: ,
+    );
+  }
+}
