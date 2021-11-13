a315b7e215b97f4ee1716436988e499e46527f88
Dreamwalker <aristojeff@gmail.com>
2021-11-13 19:37:02 +0900

    Update farmacia_home_page.dart

--- lib/ep971_farmacia_app/ui/farmacia_home_page.dart
+++ lib/ep971_farmacia_app/ui/farmacia_home_page.dart
@@ -16,100 +16,204 @@ class FarmaciaHomePage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: GetBuilder<FarmaciaScreenController>(
-              builder: ( controller) {
+              builder: (controller) {
                 return Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    controller.bottomTabIndex == 0 ? Flexible(
-                        child: GestureDetector(
-                          onTap: () {
-                            controller.setBottomTabIndex(0);
-                          },
-                          child: Container(
-                            width: 100,
-                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Row(
-                              children: const [
-                                Icon(
-                                  Icons.apps,
-                                  color: Colors.white,
-                                ),
-                                SizedBox(
-                                  width: 4,
-                                ),
-                                Text(
-                                  "Home",
-                                  style: TextStyle(
+                    controller.bottomTabIndex == 0
+                        ? Flexible(
+                            child: GestureDetector(
+                            onTap: () {
+                              controller.setBottomTabIndex(0);
+                            },
+                            child: Container(
+                              width: 100,
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Row(
+                                children: const [
+                                  Icon(
+                                    Icons.apps,
                                     color: Colors.white,
                                   ),
-                                )
-                              ],
+                                  SizedBox(
+                                    width: 4,
+                                  ),
+                                  Text(
+                                    "Home",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        )) :
-                    IconButton(
-                        onPressed: () {
-                          controller.setBottomTabIndex(0);
-                        },
-                        icon: const Icon(
-                          Icons.apps,
-                        )),
-                    controller.bottomTabIndex == 1 ? Flexible(
-                        child: GestureDetector(
-                          onTap: () {
-                            controller.setBottomTabIndex(1);
-                          },
-                          child: Container(
-                            width: 100,
-                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Row(
-                              children: const [
-                                Icon(
-                                  Icons.shopping_cart_outlined,
-                                  color: Colors.white,
-                                ),
-                                SizedBox(
-                                  width: 4,
-                                ),
-                                Text(
-                                  "Cart",
-                                  style: TextStyle(
+                          ))
+                        : IconButton(
+                            onPressed: () {
+                              controller.setBottomTabIndex(0);
+                            },
+                            icon: const Icon(
+                              Icons.apps,
+                            )),
+                    controller.bottomTabIndex == 1
+                        ? Flexible(
+                            child: GestureDetector(
+                            onTap: () {
+                              controller.setBottomTabIndex(1);
+                            },
+                            child: Container(
+                              width: 100,
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Row(
+                                children: const [
+                                  Icon(
+                                    Icons.shopping_cart_outlined,
                                     color: Colors.white,
                                   ),
-                                )
-                              ],
+                                  SizedBox(
+                                    width: 4,
+                                  ),
+                                  Text(
+                                    "Cart",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        )) :
-                    IconButton(
-                        onPressed: () { controller.setBottomTabIndex(1);},
-                        icon: const Icon(
-                          Icons.shopping_cart_outlined,
-                        )),
-
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.search,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.bookmark_border,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.account_circle_outlined,
-                        )),
+                          ))
+                        : IconButton(
+                            onPressed: () {
+                              controller.setBottomTabIndex(1);
+                            },
+                            icon: const Icon(
+                              Icons.shopping_cart_outlined,
+                            )),
+                    controller.bottomTabIndex == 2
+                        ? Flexible(
+                            child: GestureDetector(
+                            onTap: () {
+                              controller.setBottomTabIndex(2);
+                            },
+                            child: Container(
+                              // width: 100,
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Row(
+                                children: const [
+                                  Icon(
+                                    Icons.search,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    width: 4,
+                                  ),
+                                  Text(
+                                    "Search",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ))
+                        : IconButton(
+                            onPressed: () {
+                              controller.setBottomTabIndex(2);
+                            },
+                            icon: const Icon(
+                              Icons.search,
+                            )),
+                    controller.bottomTabIndex == 3
+                        ? Flexible(
+                            child: GestureDetector(
+                            onTap: () {
+                              controller.setBottomTabIndex(3);
+                            },
+                            child: Container(
+                              width: 100,
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Row(
+                                children: const [
+                                  Icon(
+                                    Icons.bookmark_border,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    width: 4,
+                                  ),
+                                  Text(
+                                    "Cart",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ))
+                        : IconButton(
+                            onPressed: () {
+                              controller.setBottomTabIndex(3);
+                            },
+                            icon: const Icon(
+                              Icons.bookmark_border,
+                            )),
+                    controller.bottomTabIndex == 4
+                        ? Flexible(
+                            child: GestureDetector(
+                            onTap: () {
+                              controller.setBottomTabIndex(4);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Row(
+                                children: const [
+                                  Icon(
+                                    Icons.account_circle_outlined,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    width: 4,
+                                  ),
+                                  Text(
+                                    "Account",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ))
+                        : IconButton(
+                            onPressed: () {
+                              controller.setBottomTabIndex(4);
+                            },
+                            icon: const Icon(
+                              Icons.account_circle_outlined,
+                            )),
                   ],
                 );
               },
