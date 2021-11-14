ddde39147fba7a0df9c93fd22261c8dd555f4d25
Dreamwalker <aristojeff@gmail.com>
2021-11-14 20:17:30 +0900

    Update farmacia_home_screen.dart

--- lib/ep971_farmacia_app/ui/farmacia_home_screen.dart
+++ lib/ep971_farmacia_app/ui/farmacia_home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_16th_story/ep971_farmacia_app/controller/home_category_tab_controller.dart';
+import 'package:get/get.dart';
 
 class FarmaciaHomeScreen extends StatelessWidget {
   const FarmaciaHomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final FarmaciaHomeCategoriesController homeCategoriesController = Get.put(FarmaciaHomeCategoriesController());
     return DefaultTabController(
       length: 3,
       child: Column(
@@ -46,6 +49,7 @@ class FarmaciaHomeScreen extends StatelessWidget {
             ),
           ),
           const TabBar(
+              physics: NeverScrollableScrollPhysics(),
               labelStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
               labelColor: Colors.black,
               indicatorColor: Colors.white,
@@ -64,95 +68,84 @@ class FarmaciaHomeScreen extends StatelessWidget {
               ]),
           Expanded(
               child: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
             children: [
               Column(
                 children: [
-                  SizedBox(
-                    height: 42,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right:4),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            decoration:  BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(4)
-                            ),
-                            child: Center(
-                              child: const Text("First aid kit",
-                              style: const TextStyle(
-                                color: Colors.white
-                              ),),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right:4),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            decoration:  BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(4)
-                            ),
-                            child: Center(
-                              child: const Text("First aid kit",
-                                style: const TextStyle(
-                                    color: Colors.white
-                                ),),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: SizedBox(
+                      height: 42,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(4)),
+                              child: const Center(
+                                child: Text(
+                                  "First aid kit",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right:4),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            decoration:  BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(4)
-                            ),
-                            child: Center(
-                              child: const Text("First aid kit",
-                                style: const TextStyle(
-                                    color: Colors.white
-                                ),),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(4)),
+                              child: const Center(
+                                child: Text(
+                                  "First aid kit",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right:4),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            decoration:  BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(4)
-                            ),
-                            child: Center(
-                              child: const Text("First aid kit",
-                                style: const TextStyle(
-                                    color: Colors.white
-                                ),),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(4)),
+                              child: const Center(
+                                child: Text(
+                                  "First aid kit",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right:4),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            decoration:  BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(4)
-                            ),
-                            child: Center(
-                              child: const Text("First aid kit",
-                                style: const TextStyle(
-                                    color: Colors.white
-                                ),),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(4)),
+                              child: const Center(
+                                child: Text(
+                                  "First aid kit",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
                             ),
                           ),
-                        )
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(4)),
+                              child: const Center(
+                                child: Text(
+                                  "First aid kit",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   )
                 ],
