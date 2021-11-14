b9f6cb61b7e0948fc6cc42287c959069f233c42c
Dreamwalker <aristojeff@gmail.com>
2021-11-14 20:05:06 +0900

    Update farmacia_home_screen.dart

--- lib/ep971_farmacia_app/ui/farmacia_home_screen.dart
+++ lib/ep971_farmacia_app/ui/farmacia_home_screen.dart
@@ -5,43 +5,63 @@ class FarmaciaHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 24,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Hello, Dream!",
-                      style: TextStyle(fontWeight: FontWeight.bold,
-                      fontSize: 16),
-                    ),
-                    Text("What are you looking for today?")
-                  ],
+    return DefaultTabController(
+      length: 3,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 24,
                 ),
-              ),
-              Container(
-                height: 32,
-                width: 32,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(4),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Hello, Dream!",
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                      ),
+                      Text("What are you looking for today?")
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  height: 32,
+                  width: 32,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(4),
 
+                  ),
+                  child: const Icon(Icons.qr_code_scanner_outlined,color: Colors.white,),
                 ),
-                child: Icon(Icons.qr_code_scanner_outlined,color: Colors.white,),
-              )
-            ],
+              ],
+            ),
           ),
-        )
-      ],
+           TabBar(
+              labelColor: Colors.black,
+              indicatorWeight: 0,
+
+
+              tabs: [
+            Tab(text: "PHARMACIES",),
+            Tab(text: "PET SUPPLIES",),
+            Tab(text: "ORTHOPEDICS",),
+          ]),
+          Expanded(child: TabBarView(
+            children: [
+              Container(),
+              Container(),
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
