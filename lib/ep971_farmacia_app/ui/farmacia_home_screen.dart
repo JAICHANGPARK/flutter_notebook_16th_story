af31209b2e88912e223241823e60280231d34fee
Dreamwalker <aristojeff@gmail.com>
2021-11-14 19:55:53 +0900

    Create farmacia_home_screen.dart

---
+++ lib/ep971_farmacia_app/ui/farmacia_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class FarmaciaHomeScreen extends StatelessWidget {
+  const FarmaciaHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
