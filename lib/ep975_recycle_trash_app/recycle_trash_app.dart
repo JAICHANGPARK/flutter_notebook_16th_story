f6e241e10efccf0493d90040fc5920439577706d
Dreamwalker <aristojeff@gmail.com>
2021-11-17 19:26:00 +0900

    Update recycle_trash_app.dart

---
+++ lib/ep975_recycle_trash_app/recycle_trash_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecycleTrashApp extends StatelessWidget {
+  const RecycleTrashApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
