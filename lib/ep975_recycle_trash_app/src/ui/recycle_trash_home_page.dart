b0357b96c613cd8d91dc27fad1f040f30d28ae29
Dreamwalker <aristojeff@gmail.com>
2021-11-17 19:27:52 +0900

    Create recycle_trash_home_page.dart

---
+++ lib/ep975_recycle_trash_app/src/ui/recycle_trash_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class RecycleTrashHomePage extends StatelessWidget {
+  const RecycleTrashHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
