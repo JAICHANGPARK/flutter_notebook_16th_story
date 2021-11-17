94813973f4df48fcfc9f9311cb6014a566d6230d
Dreamwalker <aristojeff@gmail.com>
2021-11-17 19:35:35 +0900

    Update recycle_trash_home_page.dart

--- lib/ep975_recycle_trash_app/src/ui/recycle_trash_home_page.dart
+++ lib/ep975_recycle_trash_app/src/ui/recycle_trash_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class RecycleTrashHomePage extends StatelessWidget {
   const RecycleTrashHomePage({Key? key}) : super(key: key);
 
@@ -8,24 +7,27 @@ class RecycleTrashHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-
-        ],
+        children: [],
       ),
-
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         foregroundColor: Colors.teal,
-        backgroundColor: Colors.white, onPressed: () {  },
+        backgroundColor: Colors.white,
+        onPressed: () {},
       ),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        items: [
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.teal,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "home")
         ],
-      )
-      ,
+      ),
     );
   }
 }
