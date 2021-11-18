d9f781f7462446cef64d04fd7894d0d89c74e90f
Dreamwalker <aristojeff@gmail.com>
2021-11-18 19:12:20 +0900

    Update recycle_home_screen.dart

--- lib/ep975_recycle_trash_app/src/ui/recycle_home_screen.dart
+++ lib/ep975_recycle_trash_app/src/ui/recycle_home_screen.dart
@@ -7,23 +7,29 @@ class RecycleHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            Column(
-              children: [
-                Text("Hi Dreamwalker"),
-                Text(
-                  "Welcome back!",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-            CircleAvatar(
-              child: Icon(Icons.notification),
-            )
-          ],
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Text("Hi Dreamwalker"),
+                  Text(
+                    "Welcome back!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              Spacer(),
+              CircleAvatar(
+                backgroundColor: Colors.teal[100],
+                foregroundColor: Colors.teal,
+                child: Icon(Icons.notifications_outlined),
+              )
+            ],
+          ),
         )
       ],
     );
