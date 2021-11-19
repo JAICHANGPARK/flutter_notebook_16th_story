86618212d08e869a08029fe06781f348666eb490
Dreamwalker <aristojeff@gmail.com>
2021-11-19 19:22:27 +0900

    Update trash_home_item.dart

--- lib/ep975_recycle_trash_app/src/controller/trash_home_item.dart
+++ lib/ep975_recycle_trash_app/src/controller/trash_home_item.dart
@@ -1,3 +1,10 @@
 
 
 
+import 'package:flutter_notebook_16th_story/ep975_recycle_trash_app/src/model/trash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TrashList extends StateNotifier<List<Trash>>{
+  TrashList(List<Trash> state) : super(state);
+
+}
