5d7295d164ad4be91b4c2c7001fb8ae3e86d7f5c
Dreamwalker <aristojeff@gmail.com>
2021-11-19 19:24:49 +0900

    Update trash_home_item.dart

--- lib/ep975_recycle_trash_app/src/controller/trash_home_item.dart
+++ lib/ep975_recycle_trash_app/src/controller/trash_home_item.dart
@@ -1,10 +1,13 @@
-
-
-
 import 'package:flutter_notebook_16th_story/ep975_recycle_trash_app/src/model/trash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class TrashList extends StateNotifier<List<Trash>>{
-  TrashList(List<Trash> state) : super(state);
+final trashListProvider = StateNotifierProvider<TrashList, List<Trash>>((ref) {
+  return TrashList(List.generate(
+    10,
+    (index) => Trash(title: "Sample${index}", subtitle: "12 kail"),
+  ));
+});
 
+class TrashList extends StateNotifier<List<Trash>> {
+  TrashList(List<Trash> state) : super(state);
 }
