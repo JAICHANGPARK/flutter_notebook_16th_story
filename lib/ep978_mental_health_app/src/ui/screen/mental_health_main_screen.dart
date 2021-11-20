46e606002ac6b1f49bdf8ba85294a8bb3ce1bbca
Dreamwalker <aristojeff@gmail.com>
2021-11-20 19:24:03 +0900

    Update mental_health_main_screen.dart

--- lib/ep978_mental_health_app/src/ui/screen/mental_health_main_screen.dart
+++ lib/ep978_mental_health_app/src/ui/screen/mental_health_main_screen.dart
@@ -15,23 +15,47 @@ class _MentalHealthMainScreenState extends State<MentalHealthMainScreen> {
       body: SafeArea(
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const Text(
+                    "My Spaces",
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+                  ),
+                  Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(24),
+                        border: Border.all(color: Colors.black, width: 1.5)),
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                    child: const Text("SOS"),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Coolicons.settings_future),
+                  ),
+                ],
+              ),
+            ),
             Row(
               children: [
-                const Text("My Spaces"),
                 Container(
+                  height: 48,
+                  width: 48,
                   decoration: BoxDecoration(
-                      color: Colors.yellow,
-                      borderRadius: BorderRadius.circular(24),
-                      border: Border.all(
-                        color: Colors.black,
-                      )),
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                  child: const Text("SOS"),
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.black, width: 1.5),
+                  ),
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Coolicons.settings_future),
-                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Hello Dreamwalker"),
+                    Text("Let's explore all the features here")
+                  ],
+                )
               ],
             )
           ],
