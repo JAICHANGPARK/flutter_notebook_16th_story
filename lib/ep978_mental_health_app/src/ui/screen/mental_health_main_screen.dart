311b9ace404392fd22720fe08ddc70dab3a8d126
Dreamwalker <aristojeff@gmail.com>
2021-11-20 19:17:55 +0900

    Create mental_health_main_screen.dart

---
+++ lib/ep978_mental_health_app/src/ui/screen/mental_health_main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MentalHealthMainScreen extends StatefulWidget {
+  const MentalHealthMainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MentalHealthMainScreenState createState() => _MentalHealthMainScreenState();
+}
+
+class _MentalHealthMainScreenState extends State<MentalHealthMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
