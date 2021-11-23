fa3484806ad508db9b88594540e5f19a0e2853e3
Dreamwalker <aristojeff@gmail.com>
2021-11-23 19:44:49 +0900

    Create fake_wellness.dart

---
+++ lib/ep978_mental_health_app/src/data/fake_wellness.dart
@@ -0,0 +1,41 @@
+
+
+import '../model/wellness.dart';
+
+List<Wellness> fakeWellness = [
+    Wellness(
+      img: "https://cdn.pixabay.com/photo/2021/11/17/15/07/paris-6803796__340.jpg",
+      title: "Pacing Technique",
+      time: "03:42",
+      isLock: false,
+      subtitle: "Cope With Pain"
+    ),
+  Wellness(
+      img: "https://cdn.pixabay.com/photo/2021/11/17/15/07/paris-6803796__340.jpg",
+      title: "Pacing Technique",
+      time: "03:42",
+      isLock: false,
+      subtitle: "Cope With Pain"
+  ),
+  Wellness(
+      img: "https://cdn.pixabay.com/photo/2021/11/17/15/07/paris-6803796__340.jpg",
+      title: "Pacing Technique",
+      time: "03:42",
+      isLock: true,
+      subtitle: "Cope With Pain"
+  ),
+  Wellness(
+      img: "https://cdn.pixabay.com/photo/2021/11/17/15/07/paris-6803796__340.jpg",
+      title: "Pacing Technique",
+      time: "03:42",
+      isLock: true,
+      subtitle: "Cope With Pain"
+  ),
+  Wellness(
+      img: "https://cdn.pixabay.com/photo/2021/11/17/15/07/paris-6803796__340.jpg",
+      title: "Pacing Technique",
+      time: "03:42",
+      isLock: true,
+      subtitle: "Cope With Pain"
+  )
+];
