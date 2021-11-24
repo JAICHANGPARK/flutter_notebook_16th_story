eb18687526c17457774005f77dcf009801429c62
Dreamwalker <aristojeff@gmail.com>
2021-11-24 19:49:26 +0900

    Create mental_home_component.dart

---
+++ lib/ep978_mental_health_app/src/ui/screen/mental_home_component.dart
@@ -0,0 +1,373 @@
+
+import 'package:flutter/material.dart';
+
+class MentalHomeComponent extends StatelessWidget {
+  const MentalHomeComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Text(
+                "My Spaces",
+                style: GoogleFonts.balsamiqSans(fontWeight: FontWeight.bold, color: Colors.grey),
+              ),
+              const Spacer(),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(24),
+                    border: Border.all(color: Colors.black, width: 1.5)),
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                child: Text(
+                  "SOS",
+                  style: GoogleFonts.balsamiqSans(fontWeight: FontWeight.bold),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Coolicons.settings_future),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.black, width: 1.5),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hello Dreamwalker",
+                      style: GoogleFonts.balsamiqSans(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Let's explore all the features here",
+                      style: GoogleFonts.balsamiqSans(color: Colors.grey, fontSize: 12),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Use Voice in Menta",
+                style: GoogleFonts.balsamiqSans(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.cyan[100],
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(color: Colors.black, width: 1.5)),
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "New Features",
+                          style: GoogleFonts.balsamiqSans(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Talk through your anxiety",
+                          style: GoogleFonts.balsamiqSans(fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Container(
+                      height: 48,
+                      width: 48,
+                      decoration: const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                      padding: const EdgeInsets.all(4),
+                      child: Container(
+                        decoration:
+                        BoxDecoration(shape: BoxShape.circle, border: Border.all(color: Colors.black)),
+                        child: const Center(
+                          child: Icon(Icons.mic),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              const Text("Self Care"),
+              const Spacer(),
+              Column(
+                children: [
+                  const Text("See All"),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Container(
+                    height: 2,
+                    width: 42,
+                    decoration: const BoxDecoration(color: Colors.blue),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 54,
+                        width: 54,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.black,
+                            )),
+                        padding: const EdgeInsets.all(2),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.blue,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "Talk",
+                        style: GoogleFonts.balsamiqSans(
+                          fontWeight: FontWeight.w300,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            "My 4 Am Friends",
+            style: GoogleFonts.balsamiqSans(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            height: 110,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Container(
+                  height: 84,
+                  width: 84,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2020/11/23/18/18/birds-5770589_960_720.jpg",
+                        ),
+                        fit: BoxFit.fitHeight,
+                      )),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "How are you feeling?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.yellow,
+                          border: Border.all(color: Colors.black, width: 1.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                      child: const Center(
+                        child: Text(
+                          "Talk Now",
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Consumer(
+            builder: (context, ref, _) {
+              final index = ref.watch(mhBottomTabProvider);
+              return Container(
+                height: 64,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(36),
+                    border: Border.all(color: Colors.black, width: 1.5)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    index == 0
+                        ? GestureDetector(
+                      onTap: () {
+                        ref.read(mhBottomTabProvider.notifier).state = 0;
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(color: Colors.black, width: 1.5)),
+                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                        child: const Icon(Ionicons.home),
+                      ),
+                    )
+                        : IconButton(
+                      onPressed: () => ref.read(mhBottomTabProvider.notifier).state = 0,
+                      icon: const Icon(Ionicons.home),
+                      color: Colors.white,
+                    ),
+                    index == 1
+                        ? GestureDetector(
+                      onTap: () {
+                        ref.read(mhBottomTabProvider.notifier).state = 0;
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(color: Colors.black, width: 1.5)),
+                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                        child: const Icon(Ionicons.chatbubble_outline),
+                      ),
+                    )
+                        : IconButton(
+                      onPressed: () => ref.read(mhBottomTabProvider.notifier).state = 1,
+                      icon: const Icon(Ionicons.chatbubble_outline),
+                      color: Colors.white,
+                    ),
+                    index == 2
+                        ? GestureDetector(
+                      onTap: () {
+                        ref.read(mhBottomTabProvider.notifier).state = 0;
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(color: Colors.black, width: 1.5)),
+                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                        child: const Icon(Ionicons.apps_outline),
+                      ),
+                    )
+                        : IconButton(
+                      onPressed: () => ref.read(mhBottomTabProvider.notifier).state = 2,
+                      icon: const Icon(Ionicons.apps_outline),
+                      color: Colors.white,
+                    ),
+                    index == 3
+                        ? GestureDetector(
+                      onTap: () {
+                        ref.read(mhBottomTabProvider.notifier).state = 0;
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(color: Colors.black, width: 1.5)),
+                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                        child: const Icon(Ionicons.document_text_outline),
+                      ),
+                    )
+                        : IconButton(
+                      onPressed: () => ref.read(mhBottomTabProvider.notifier).state = 3,
+                      icon: const Icon(Ionicons.document_text_outline),
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
