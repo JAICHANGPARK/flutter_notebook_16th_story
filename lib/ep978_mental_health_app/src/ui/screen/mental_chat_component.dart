990db20ac59537c8f0846b98e4a1f054f2ab06ee
Dreamwalker <aristojeff@gmail.com>
2021-11-25 20:46:39 +0900

    Update mental_chat_component.dart

--- lib/ep978_mental_health_app/src/ui/screen/mental_chat_component.dart
+++ lib/ep978_mental_health_app/src/ui/screen/mental_chat_component.dart
@@ -64,31 +64,39 @@ class MentalChatComponent extends StatelessWidget {
                 return Row(
                   children: [
                     Expanded(
-                      child: topTab == 0
-                          ? Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.yellow,
-                                  border: Border.all(
-                                    color: Colors.black,
-                                    width: 1.5,
-                                  ),
-                                  borderRadius: BorderRadius.circular(24)),
-                              child: const Center(
-                                child: Text(
-                                  "Consult",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
+                      child:
+                      GestureDetector(
+                        onTap: (){
+                          ref.read(mhChatTabProvider.notifier).state = 1;
+                        },
+                        child: topTab == 0
+                            ? Container(
+                          decoration: BoxDecoration(
+                              color: Colors.yellow,
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 1.5,
                               ),
-                            )
-                          : const Center(
-                              child: Text(
-                                "Consult",
-                                style: TextStyle(color: Colors.white),
+                              borderRadius: BorderRadius.circular(24)),
+                          child: const Center(
+                            child: Text(
+                              "Consult",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
+                          ),
+                        )
+                            : const Center(
+                          child: Text(
+                            "Consult",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+
+
                     ),
                     const SizedBox(
                       width: 12,
