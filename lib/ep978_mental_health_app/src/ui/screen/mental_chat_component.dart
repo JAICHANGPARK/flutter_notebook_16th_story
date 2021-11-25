59bc88c8fd1b29926652fba19465eadd4beb90df
Dreamwalker <aristojeff@gmail.com>
2021-11-25 20:35:47 +0900

    Update mental_chat_component.dart

---
+++ lib/ep978_mental_health_app/src/ui/screen/mental_chat_component.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../riverpod/bottom_tab_provider.dart';
+
+
+class MentalChatComponent extends StatelessWidget {
+  const MentalChatComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 24,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Consumer(
+                builder: (context, ref, _) {
+                  return GestureDetector(
+                    onTap: () {
+                      ref
+                          .read(mhBottomTabProvider.notifier)
+                          .state = 0;
+                    },
+                    child: Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1.5,
+                        ),
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const Text("Wellness"),
+              const Icon(Icons.more_horiz)
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
