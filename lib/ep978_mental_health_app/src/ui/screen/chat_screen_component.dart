f0874430ccdb09820e04aea13d4d1ef03fa1240b
Dreamwalker <aristojeff@gmail.com>
2021-11-26 20:12:37 +0900

    Update chat_screen_component.dart

--- lib/ep978_mental_health_app/src/ui/screen/chat_screen_component.dart
+++ lib/ep978_mental_health_app/src/ui/screen/chat_screen_component.dart
@@ -2,3 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
+class ChatScreenComponent extends StatelessWidget {
+  const ChatScreenComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
