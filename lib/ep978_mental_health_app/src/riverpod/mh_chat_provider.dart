85af1135229134413e0c407e2a5b8ba1fe77aa0f
Dreamwalker <aristojeff@gmail.com>
2021-11-26 20:38:46 +0900

    Update mh_chat_provider.dart

--- lib/ep978_mental_health_app/src/riverpod/mh_chat_provider.dart
+++ lib/ep978_mental_health_app/src/riverpod/mh_chat_provider.dart
@@ -1,8 +1,15 @@
+import 'package:flutter_notebook_16th_story/ep978_mental_health_app/src/riverpod/chat_text_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final mhChatProvider = StateNotifierProvider<ChatList, List<String>>((ref) => ChatList([], ref.read));
 
 class ChatList extends StateNotifier<List<String>> {
-  Reader ref;
-  ChatList(List<String> state, this.ref) : super(state);
+  Reader read;
+
+  ChatList(List<String> state, this.read) : super(state);
+
+  add(String message, bool isSend) {
+    String msg = read(chatTextProvider).text.trim();
+    print("add: msg: $msg");
+  }
 }
