827881ec8c4a3539db468c98e9c8a1316d8a1aaa
Dreamwalker <aristojeff@gmail.com>
2021-11-26 20:14:07 +0900

    Update chat_screen_component.dart

--- lib/ep978_mental_health_app/src/ui/screen/chat_screen_component.dart
+++ lib/ep978_mental_health_app/src/ui/screen/chat_screen_component.dart
@@ -7,16 +7,24 @@ class ChatScreenComponent extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    return Column(
-      children: [
-        Expanded(child: Placeholder()),
-        Container(
-          height: 72,
-          decoration: BoxDecoration(
-            color: Colors.yellow
-          ),
-        )
-      ],
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+          border: Border.all(
+              color: Colors.black,
+              width: 1.5)
+      ),
+      child: Column(
+        children: [
+          Expanded(child: Placeholder()),
+          Container(
+            height: 72,
+            decoration: BoxDecoration(
+              color: Colors.yellow
+            ),
+          )
+        ],
+      ),
     );
   }
 }
