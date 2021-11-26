9d9548a2d456f6991b0844b90a6a1634a89576e5
Dreamwalker <aristojeff@gmail.com>
2021-11-26 20:22:43 +0900

    Create chat_text_provider.dart

---
+++ lib/ep978_mental_health_app/src/riverpod/chat_text_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final chatTextProvider = Provider((ref){
+  TextEditingController _textEditingController = TextEditingController();
+  return _textEditingController;
+});
