ac1e22ac6f658f10600bca002d06c3b15e4505dd
Dreamwalker <aristojeff@gmail.com>
2021-11-28 20:03:46 +0900

    Update mh_chat_provider.dart

--- lib/ep978_mental_health_app/src/riverpod/mh_chat_provider.dart
+++ lib/ep978_mental_health_app/src/riverpod/mh_chat_provider.dart
@@ -1,12 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter_notebook_16th_story/ep978_mental_health_app/src/model/mh_chat.dart';
 import 'package:flutter_notebook_16th_story/ep978_mental_health_app/src/riverpod/chat_text_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 final mhChatProvider = StateNotifierProvider<ChatList, List<MhChat>>((ref) => ChatList([], ref.read));
 
 class ChatList extends StateNotifier<List<MhChat>> {
   Reader read;
+  Timer? receiveTimer;
 
   ChatList(List<MhChat> state, this.read) : super(state);
 
@@ -20,7 +24,21 @@ class ChatList extends StateNotifier<List<MhChat>> {
           profileImg: "https://cdn.pixabay.com/photo/2021/11/15/05/52/red-fox-6796430_960_720.jpg",
           isSend: true,
           msg: msg,
+          time: DateFormat("HH:mm").format(DateTime.now()),
           uid: const Uuid().v4())
     ];
+    receiveTimer ??= Timer(const Duration(milliseconds: 300), () {
+      state = [
+        ...state,
+        MhChat(
+            profileImg: "https://cdn.pixabay.com/photo/2021/11/15/05/52/red-fox-6796430_960_720.jpg",
+            isSend: false,
+            msg: "Copy that.",
+            time: DateFormat("HH:mm").format(DateTime.now()),
+            uid: const Uuid().v4())
+      ];
+      receiveTimer?.cancel();
+      receiveTimer = null;
+    });
   }
 }
