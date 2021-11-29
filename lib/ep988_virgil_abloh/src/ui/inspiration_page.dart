0c47decb12481935dba78a9789f8c7c6b8fb88b6
Dreamwalker <aristojeff@gmail.com>
2021-11-29 19:20:05 +0900

    Update inspiration_page.dart

--- lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
+++ lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
@@ -2,8 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 List<String> quotes = [
-  "Wow. I would definitely say it's gonna die you know?"
-      "Fashion and music are two great artistic forms that can be molded by the youth culture - our taste and our passion for evolving things in our limited time on earth allows us to look at things with fresh eyes.",
+  "Wow. I would definitely say it's gonna die you know?",
+  "Fashion and music are two great artistic forms that can be molded by the youth culture - our taste and our passion for evolving things in our limited time on earth allows us to look at things with fresh eyes.",
 ];
 
 class InspirationPage extends StatefulWidget {
@@ -31,11 +31,33 @@ class _InspirationPageState extends State<InspirationPage> {
                 imageUrl:
                     "https://upload.wikimedia.org/wikipedia/commons/3/31/Virgil_Abloh_Paris_Fashion_Week_Autumn_Winter_2019_%28cropped%29.jpg",
               )),
+
+          Positioned(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("\"STREETWEAR IN THE 2020S?\""),
+                SizedBox(height: 24,),
+                Center(
+                    child: Text(
+                  quotes[0].toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 24
+                  ),
+                )),
+              ],
+            ),
+            left: 24,
+            right: 24,
+            bottom: 0,
+            top: 0,
+          ),
           const Positioned(
-            child: Center(child: Text("Virgil Abloh",style: TextStyle(
-              color: Colors.white,
-              fontSize: 18
-            ),)),
+            child: Center(
+                child: Text(
+              "Virgil Abloh",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            )),
             bottom: 24,
             left: 0,
             right: 0,
