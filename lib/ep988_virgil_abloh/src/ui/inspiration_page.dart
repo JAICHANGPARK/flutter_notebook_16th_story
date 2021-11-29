177e7dcf3633a23191cff52abe3db4d646447628
Dreamwalker <aristojeff@gmail.com>
2021-11-29 19:13:06 +0900

    Update inspiration_page.dart

--- lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
+++ lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
@@ -1,5 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
 
 class InspirationPage extends StatefulWidget {
   const InspirationPage({Key? key}) : super(key: key);
@@ -11,6 +11,20 @@ class InspirationPage extends StatefulWidget {
 class _InspirationPageState extends State<InspirationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: CachedNetworkImage(
+            imageUrl:
+                "https://upload.wikimedia.org/wikipedia/commons/3/31/Virgil_Abloh_Paris_Fashion_Week_Autumn_Winter_2019_%28cropped%29.jpg",
+          ))
+        ],
+      ),
+    );
   }
 }
