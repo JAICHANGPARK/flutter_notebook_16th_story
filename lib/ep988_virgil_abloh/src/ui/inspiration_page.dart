2e89a08d1c402b5658b9adf66c79be36288458bf
Dreamwalker <aristojeff@gmail.com>
2021-11-29 19:15:56 +0900

    Update inspiration_page.dart

--- lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
+++ lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
@@ -1,6 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+List<String> quotes = [
+  "Wow. I would definitely say it's gonna die you know?"
+  "Fashion and music are two great artistic forms that can be molded by the youth culture - our taste and our passion for evolving things in our limited time on earth allows us to look at things with fresh eyes.",
+
+];
+
 class InspirationPage extends StatefulWidget {
   const InspirationPage({Key? key}) : super(key: key);
 
