52c1a4c6f49046f2a1cc341cf238fc10c4fdac14
Dreamwalker <aristojeff@gmail.com>
2021-11-29 19:17:24 +0900

    Update inspiration_page.dart

--- lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
+++ lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 
 List<String> quotes = [
   "Wow. I would definitely say it's gonna die you know?"
-  "Fashion and music are two great artistic forms that can be molded by the youth culture - our taste and our passion for evolving things in our limited time on earth allows us to look at things with fresh eyes.",
-
+      "Fashion and music are two great artistic forms that can be molded by the youth culture - our taste and our passion for evolving things in our limited time on earth allows us to look at things with fresh eyes.",
 ];
 
 class InspirationPage extends StatefulWidget {
@@ -21,17 +20,23 @@ class _InspirationPageState extends State<InspirationPage> {
       body: Stack(
         children: [
           Positioned(
-            left: 0,
+              left: 0,
               right: 0,
               top: 0,
               bottom: 0,
               child: CachedNetworkImage(
                 fit: BoxFit.cover,
-            color: Colors.black.withOpacity(0.5),
-            colorBlendMode: BlendMode.darken,
-            imageUrl:
-                "https://upload.wikimedia.org/wikipedia/commons/3/31/Virgil_Abloh_Paris_Fashion_Week_Autumn_Winter_2019_%28cropped%29.jpg",
-          ))
+                color: Colors.black.withOpacity(0.5),
+                colorBlendMode: BlendMode.darken,
+                imageUrl:
+                    "https://upload.wikimedia.org/wikipedia/commons/3/31/Virgil_Abloh_Paris_Fashion_Week_Autumn_Winter_2019_%28cropped%29.jpg",
+              )),
+          const Positioned(
+            child: Center(child: Text("Virgil Abloh")),
+            bottom: 16,
+            left: 0,
+            right: 0,
+          ),
         ],
       ),
     );
