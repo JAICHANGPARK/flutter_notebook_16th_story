750f5be27b1f380e707cbe7af8ea732e6dfcbb59
Dreamwalker <aristojeff@gmail.com>
2021-11-29 19:11:23 +0900

    Create inspiration_page.dart

---
+++ lib/ep988_virgil_abloh/src/ui/inspiration_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class InspirationPage extends StatefulWidget {
+  const InspirationPage({Key? key}) : super(key: key);
+
+  @override
+  _InspirationPageState createState() => _InspirationPageState();
+}
+
+class _InspirationPageState extends State<InspirationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
