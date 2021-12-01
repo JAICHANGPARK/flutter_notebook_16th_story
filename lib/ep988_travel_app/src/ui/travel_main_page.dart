a914f278d90ebaaa301564622380c0a282cf1719
Dreamwalker <aristojeff@gmail.com>
2021-12-01 19:48:21 +0900

    Update travel_main_page.dart

--- lib/ep988_travel_app/src/ui/travel_main_page.dart
+++ lib/ep988_travel_app/src/ui/travel_main_page.dart
@@ -72,29 +72,45 @@ class _TravelMainPageState extends State<TravelMainPage> {
                       child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        width: 240,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: 8,
-                              top: 4,
-                              bottom: 4,
-                              right: 8,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16),
-                                  color: Colors.blueGrey,
-                                  image: const DecorationImage(
-                                    image: CachedNetworkImageProvider(
-                                      "https://cdn.pixabay.com/photo/2020/12/17/00/43/skyscrapers-5838029__340.jpg",
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: Container(
+                          width: 240,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 4,
+                                top: 4,
+                                bottom: 4,
+                                right: 8,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(16),
+                                    color: Colors.blueGrey,
+                                    image: const DecorationImage(
+                                      image: CachedNetworkImageProvider(
+                                        "https://cdn.pixabay.com/photo/2020/12/17/00/43/skyscrapers-5838029__340.jpg",
+                                      ),
+                                      fit: BoxFit.cover,
                                     ),
-                                    fit: BoxFit.cover,
                                   ),
                                 ),
                               ),
-                            )
-                          ],
+                              Positioned(
+                                  top: 24,
+                                  left: 0,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.black87,
+                                    ),
+                                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                                    child: Text(
+                                      "06 days 16 hours",
+                                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                                    ),
+                                  ))
+                            ],
+                          ),
                         ),
                       );
                     },
