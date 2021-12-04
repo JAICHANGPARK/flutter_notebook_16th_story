29f8c4c8f6005ea712cdae3780a4597299b0aa64
Dreamwalker <aristojeff@gmail.com>
2021-12-04 20:03:09 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'ep988_travel_app/travel_app.dart';
+import 'ep992_twist_app/twist_app.dart';
 
 main() {
-  runApp(const TravelApp());
+  runApp(const TwistApp());
 }
