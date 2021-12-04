eeea55873eb93871ec440cf69172a6bf3cc53241
Dreamwalker <aristojeff@gmail.com>
2021-12-04 20:03:03 +0900

    Update twist_home_page.dart

---
+++ lib/ep992_twist_app/src/ui/twist_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class TwistHomePage extends StatefulWidget {
+  const TwistHomePage({Key? key}) : super(key: key);
+
+  @override
+  _TwistHomePageState createState() => _TwistHomePageState();
+}
+
+class _TwistHomePageState extends State<TwistHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
