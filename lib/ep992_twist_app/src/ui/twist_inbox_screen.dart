b106e509e127833a9d4eae6f7b1b34253478cae2
Dreamwalker <aristojeff@gmail.com>
2021-12-04 20:12:23 +0900

    Update twist_inbox_screen.dart

--- lib/ep992_twist_app/src/ui/twist_inbox_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_inbox_screen.dart
@@ -1,1 +1,17 @@
 
+import 'package:flutter/material.dart';
+
+
+class TwistInboxScreen extends StatefulWidget {
+  const TwistInboxScreen({Key? key}) : super(key: key);
+
+  @override
+  _TwistInboxScreenState createState() => _TwistInboxScreenState();
+}
+
+class _TwistInboxScreenState extends State<TwistInboxScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
