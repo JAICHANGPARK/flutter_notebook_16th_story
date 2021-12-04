c90c7be3b59d79684daf5efb452c5f61e930c090
Dreamwalker <aristojeff@gmail.com>
2021-12-04 20:12:21 +0900

    Update twist_home_page.dart

--- lib/ep992_twist_app/src/ui/twist_home_page.dart
+++ lib/ep992_twist_app/src/ui/twist_home_page.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_16th_story/ep992_twist_app/src/controller/twist_menu_contoller.dart';
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
 
-
-class TwistHomePage extends StatefulWidget {
+class TwistHomePage extends StatelessWidget {
   const TwistHomePage({Key? key}) : super(key: key);
 
-  @override
-  _TwistHomePageState createState() => _TwistHomePageState();
-}
-
-class _TwistHomePageState extends State<TwistHomePage> {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<TwistMenuController>(
@@ -21,9 +16,22 @@ class _TwistHomePageState extends State<TwistHomePage> {
             index: controller.bottomTab,
 
           ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: controller.bottomTab,
+            onTap: (idx){
+              controller.setBottomTab(idx);
+            },
+            type: BottomNavigationBarType.fixed,
+            items: const [BottomNavigationBarItem(icon: Icon(Ionicons.cube_outline), label: "Inbox"),
+              BottomNavigationBarItem(icon: Icon(Icons.tag), label: "Channels"),
+              BottomNavigationBarItem(icon: Icon(Ionicons.chatbubble_outline), label: "Messages"),
+              BottomNavigationBarItem(icon: CircleAvatar(), label: "Inbox"),],
+          ),
         );
       },
 
     );
   }
 }
+
+
