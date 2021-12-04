1e865c857e85fa64b88d7b60cb34387fd30aed0a
Dreamwalker <aristojeff@gmail.com>
2021-12-04 20:07:26 +0900

    Update twist_menu_contoller.dart

--- lib/ep992_twist_app/src/controller/twist_menu_contoller.dart
+++ lib/ep992_twist_app/src/controller/twist_menu_contoller.dart
@@ -1,7 +1,10 @@
-
-
 import 'package:get/get.dart';
 
-class TwistMenuController extends GetxController{
-  
+class TwistMenuController extends GetxController {
+  int _bottomTab = 0;
+  int get bottomTab => _bottomTab;
+  setBottomTab(int i) {
+    _bottomTab = i;
+    update();
+  }
 }
