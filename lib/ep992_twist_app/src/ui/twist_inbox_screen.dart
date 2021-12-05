4a0d69bd84ac77dd0f0e9dbc115152c42d3a9f44
Dreamwalker <aristojeff@gmail.com>
2021-12-05 18:02:00 +0900

    Update twist_inbox_screen.dart

--- lib/ep992_twist_app/src/ui/twist_inbox_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_inbox_screen.dart
@@ -10,17 +10,23 @@ class TwistInboxScreen extends StatefulWidget {
 class _TwistInboxScreenState extends State<TwistInboxScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
           children: [
-            Text("Inbox"),
-            IconButton(onPressed: () {}, icon: Icon(Icons.filter)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.filter)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.filter)),
+            Row(
+              children: [
+                Text("Inbox"),
+                Spacer(),
+                IconButton(onPressed: () {}, icon: Icon(Icons.filter_list)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+              ],
+            )
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }
