dab3a97d55361ca0288ab97f1599fa8330fe64a5
Dreamwalker <aristojeff@gmail.com>
2021-12-05 18:01:58 +0900

    Update twist_inbox_controller.dart

--- lib/ep992_twist_app/src/controller/twist_inbox_controller.dart
+++ lib/ep992_twist_app/src/controller/twist_inbox_controller.dart
@@ -1,7 +1,38 @@
-
-
 import 'package:get/get.dart';
 
-class TwistInboxController extends GetxController{
+import '../model/twist_inbox.dart';
 
+class TwistInboxController extends GetxController {
+  List<TwistInbox> _inboxItems = [
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+    ),
+  ];
+
+  List<TwistInbox> get inboxItems => _inboxItems;
 }
