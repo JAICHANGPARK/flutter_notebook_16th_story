67ea9f4d1c2fb151be295f74dbd3fd591ae9daaa
Dreamwalker <aristojeff@gmail.com>
2021-12-06 19:41:42 +0900

    Update twist_channel_screen.dart

--- lib/ep992_twist_app/src/ui/twist_channel_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_channel_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_16th_story/ep992_twist_app/src/controller/twist_channel_controller.dart';
+import 'package:flutter_notebook_16th_story/ep992_twist_app/src/ui/channel_component.dart';
 import 'package:get/get.dart';
+
+import 'inbox_component.dart';
 
 class TwistChannelScreen extends StatelessWidget {
   const TwistChannelScreen({Key? key}) : super(key: key);
@@ -15,22 +18,39 @@ class TwistChannelScreen extends StatelessWidget {
             children: [
               IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.arrow_back),
+                icon: const Icon(Icons.arrow_back),
               ),
-              Spacer(),
-              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              const Spacer(),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
               IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
             ],
           ),
-          Text("Lounge"),
-          Text("Public - 10 members"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Lounge"),
+                const Text("Public - 10 members"),
+              ],
+            ),
+          ),
+
           Expanded(
               child: GetBuilder<TwistChannelController>(
             init: TwistChannelController(),
             builder: (c) {
-              return ListView.builder(
-                itemBuilder: (context, index) {},
-                itemCount: c.inboxItems.length,
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    var item = c.inboxItems[index];
+                    return ChannelComponent(
+                      twistChannel: item,
+                    );
+                  },
+                  itemCount: c.inboxItems.length,
+                ),
               );
             },
           ))
