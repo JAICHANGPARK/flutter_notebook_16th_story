b2b02751c8344300de4a05bd60f3f197e771ed92
Dreamwalker <aristojeff@gmail.com>
2021-12-06 19:24:03 +0900

    Update twist_inbox_controller.dart

--- lib/ep992_twist_app/src/controller/twist_inbox_controller.dart
+++ lib/ep992_twist_app/src/controller/twist_inbox_controller.dart
@@ -9,30 +9,64 @@ class TwistInboxController extends GetxController {
       subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
       tag: "Development",
       img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
-      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+      dateTime: DateTime.now().subtract(const Duration(minutes: 15)),
     ),
     TwistInbox(
       title: "New settings enabled",
       subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
       tag: "Development",
       img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
-      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+      dateTime: DateTime.now().subtract(const Duration(minutes: 20)),
     ),
     TwistInbox(
       title: "New settings enabled",
       subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
       tag: "Development",
       img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
-      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+      dateTime: DateTime.now().subtract(const Duration(minutes: 120)),
     ),
     TwistInbox(
       title: "New settings enabled",
       subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
       tag: "Development",
       img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
-      dateTime: DateTime.now().subtract(Duration(minutes: 15)),
+      dateTime: DateTime.now().subtract(const Duration(minutes: 30)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(const Duration(minutes: 15)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(const Duration(minutes: 20)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(const Duration(minutes: 120)),
+    ),
+    TwistInbox(
+      title: "New settings enabled",
+      subtitle: "John: Ok! That's super nice. For now let's move forward with this until someone blah...",
+      tag: "Development",
+      img: "https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_960_720.jpg",
+      dateTime: DateTime.now().subtract(const Duration(minutes: 30)),
     ),
   ];
 
   List<TwistInbox> get inboxItems => _inboxItems;
+
+  addItem(TwistInbox twistInbox) {
+    _inboxItems.add(twistInbox);
+    // _inboxItems = _inboxItems.reversed.toList();
+    update();
+  }
 }
