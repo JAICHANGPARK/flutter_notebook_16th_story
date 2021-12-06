34f6df134bfd257fa85ceb6dcd2bb354e203f921
Dreamwalker <aristojeff@gmail.com>
2021-12-06 19:41:38 +0900

    Update twist_inbox_screen.dart

--- lib/ep992_twist_app/src/ui/twist_inbox_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_inbox_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_notebook_16th_story/ep992_twist_app/src/controller/twist
 import 'package:get/get.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
+import 'inbox_component.dart';
+
 class TwistInboxScreen extends StatelessWidget {
   const TwistInboxScreen({Key? key}) : super(key: key);
 
@@ -31,85 +33,89 @@ class TwistInboxScreen extends StatelessWidget {
                 IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
               ],
             ),
-            Expanded(child: GetBuilder<TwistInboxController>(
+            Expanded(
+                child: GetBuilder<TwistInboxController>(
               init: TwistInboxController(),
               builder: (c) {
                 return ListView.builder(
                   itemCount: c.inboxItems.length,
                   itemBuilder: (context, index) {
                     var item = c.inboxItems[index];
-                    return  Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      item.title ?? "-",
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 4,
-                                    ),
-                                    Text(
-                                      item.subtitle ?? "-",
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,
-                                      style: const TextStyle(color: Colors.grey, fontSize: 12),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 8,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    timeago.format(item.dateTime ?? DateTime.now()),
-                                    style: const TextStyle(fontSize: 12),
-                                  ),
-                                  const SizedBox(
-                                    height: 4,
-                                  ),
-                                  CircleAvatar(
-                                    radius: 14,
-                                    backgroundImage: CachedNetworkImageProvider(item.img ?? ""),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 8,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              border: Border.all(color: Colors.grey),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  "# ${item.tag}",
-                                  style: const TextStyle(fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                    return InboxComponent(
+                      twistInbox: item,
                     );
+                    // return  Padding(
+                    //   padding: const EdgeInsets.symmetric(vertical: 8),
+                    //   child: Column(
+                    //     crossAxisAlignment: CrossAxisAlignment.start,
+                    //     children: [
+                    //       Row(
+                    //         children: [
+                    //           Expanded(
+                    //             child: Column(
+                    //               crossAxisAlignment: CrossAxisAlignment.start,
+                    //               children: [
+                    //                 Text(
+                    //                   item.title ?? "-",
+                    //                   style: const TextStyle(
+                    //                     fontWeight: FontWeight.bold,
+                    //                   ),
+                    //                 ),
+                    //                 const SizedBox(
+                    //                   height: 4,
+                    //                 ),
+                    //                 Text(
+                    //                   item.subtitle ?? "-",
+                    //                   overflow: TextOverflow.ellipsis,
+                    //                   maxLines: 2,
+                    //                   style: const TextStyle(color: Colors.grey, fontSize: 12),
+                    //                 ),
+                    //               ],
+                    //             ),
+                    //           ),
+                    //           const SizedBox(
+                    //             width: 8,
+                    //           ),
+                    //           Column(
+                    //             crossAxisAlignment: CrossAxisAlignment.end,
+                    //             children: [
+                    //               Text(
+                    //                 timeago.format(item.dateTime ?? DateTime.now()),
+                    //                 style: const TextStyle(fontSize: 12),
+                    //               ),
+                    //               const SizedBox(
+                    //                 height: 4,
+                    //               ),
+                    //               CircleAvatar(
+                    //                 radius: 14,
+                    //                 backgroundImage: CachedNetworkImageProvider(item.img ?? ""),
+                    //               )
+                    //             ],
+                    //           ),
+                    //         ],
+                    //       ),
+                    //       const SizedBox(
+                    //         height: 8,
+                    //       ),
+                    //       Container(
+                    //         decoration: BoxDecoration(
+                    //           borderRadius: BorderRadius.circular(16),
+                    //           border: Border.all(color: Colors.grey),
+                    //         ),
+                    //         padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                    //         child: Row(
+                    //           mainAxisSize: MainAxisSize.min,
+                    //           children: [
+                    //             Text(
+                    //               "# ${item.tag}",
+                    //               style: const TextStyle(fontSize: 12),
+                    //             ),
+                    //           ],
+                    //         ),
+                    //       )
+                    //     ],
+                    //   ),
+                    // );
                   },
                 );
               },
