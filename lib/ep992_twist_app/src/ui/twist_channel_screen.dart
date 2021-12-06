95a2f0e102ab04d2519dcf510e1b02cde2ccfabb
Dreamwalker <aristojeff@gmail.com>
2021-12-06 19:29:46 +0900

    Update twist_channel_screen.dart

---
+++ lib/ep992_twist_app/src/ui/twist_channel_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class TwistChannelScreen extends StatelessWidget {
+  const TwistChannelScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [],
+        )
+      ],
+    );
+  }
+}
