e804d7f5dd41cacfa269540b51ea64641223edce
Dreamwalker <aristojeff@gmail.com>
2021-12-06 19:51:44 +0900

    Update twist_message_screen.dart

--- lib/ep992_twist_app/src/ui/twist_message_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_message_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
-
 class TwistMessageScreen extends StatelessWidget {
   const TwistMessageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text("Messages"),
+              Spacer(),
+              IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+            ],
+          )
 
-    ],));
+        ],
+      ),
+    );
   }
 }
