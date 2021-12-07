8beb72af2a94c798364bf2a89bf6c0d5434ac387
Dreamwalker <aristojeff@gmail.com>
2021-12-07 19:55:16 +0900

    Update twist_message_controller.dart

--- lib/ep992_twist_app/src/controller/twist_message_controller.dart
+++ lib/ep992_twist_app/src/controller/twist_message_controller.dart
@@ -1,38 +1,32 @@
 import 'package:flutter_notebook_16th_story/ep992_twist_app/src/model/twist_message.dart';
 import 'package:get/get.dart';
 
-class TwistMessageController extends GetxController{
+class TwistMessageController extends GetxController {
   final List<TwistMessage> _messageItem = [
     TwistMessage(
-      name: "Generalist",
-      subtitle: "John: OK! That's super nice. Let's more flutter development",
-      dateTime: DateTime.now().subtract(Duration(minutes: 13)),
-      images: [
-        "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
-        "https://cdn.pixabay.com/photo/2016/07/22/16/54/portrait-1535266_960_720.jpg",
-      ]
-    ),
+        name: "Generalist",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 13)),
+        images: [
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+          "https://cdn.pixabay.com/photo/2016/07/22/16/54/portrait-1535266_960_720.jpg",
+        ]),
     TwistMessage(
         name: "Ada",
         subtitle: "John: OK! That's super nice. Let's more flutter development",
-        dateTime: DateTime.now().subtract(Duration(minutes: 52)),
+        dateTime: DateTime.now().subtract(const Duration(minutes: 52)),
         images: [
           "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
-
-        ]
-    ),
+        ]),
     TwistMessage(
-        name: "Ada",
+        name: "Product Design Group",
         subtitle: "John: OK! That's super nice. Let's more flutter development",
-        dateTime: DateTime.now().subtract(Duration(minutes: 52)),
+        dateTime: DateTime.now().subtract(const Duration(minutes: 52)),
         images: [
+          "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
           "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
-
-        ]
-    ),
+        ]),
   ];
 
   List<TwistMessage> get messageItem => _messageItem;
-
-
 }
