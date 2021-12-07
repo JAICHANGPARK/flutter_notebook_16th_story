9a4b7eb4a12db16e683b8954b9165a8d7e062fb5
Dreamwalker <aristojeff@gmail.com>
2021-12-07 19:52:11 +0900

    Create twist_message_controller.dart

---
+++ lib/ep992_twist_app/src/controller/twist_message_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_notebook_16th_story/ep992_twist_app/src/model/twist_message.dart';
+import 'package:get/get.dart';
+
+class TwistMessageController extends GetxController{
+  final List<TwistMessage> _messageItem = [
+    
+  ];
+
+  List<TwistMessage> get messageItem => _messageItem;
+
+
+}
