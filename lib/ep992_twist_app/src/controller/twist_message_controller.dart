d613912c3bb7356c1a5fa3694ee2a4ee13b71b4d
Dreamwalker <aristojeff@gmail.com>
2021-12-07 20:07:41 +0900

    Update twist_message_controller.dart

--- lib/ep992_twist_app/src/controller/twist_message_controller.dart
+++ lib/ep992_twist_app/src/controller/twist_message_controller.dart
@@ -26,6 +26,52 @@ class TwistMessageController extends GetxController {
           "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
           "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
         ]),
+    TwistMessage(
+        name: "Generalist",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 13)),
+        images: [
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+          "https://cdn.pixabay.com/photo/2016/07/22/16/54/portrait-1535266_960_720.jpg",
+        ]),
+    TwistMessage(
+        name: "Ada",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 52)),
+        images: [
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+        ]),
+    TwistMessage(
+        name: "Product Design Group",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 52)),
+        images: [
+          "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+        ]),
+    TwistMessage(
+        name: "Generalist",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 13)),
+        images: [
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+          "https://cdn.pixabay.com/photo/2016/07/22/16/54/portrait-1535266_960_720.jpg",
+        ]),
+    TwistMessage(
+        name: "Ada",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 52)),
+        images: [
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+        ]),
+    TwistMessage(
+        name: "Product Design Group",
+        subtitle: "John: OK! That's super nice. Let's more flutter development",
+        dateTime: DateTime.now().subtract(const Duration(minutes: 52)),
+        images: [
+          "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
+          "https://cdn.pixabay.com/photo/2014/09/25/22/14/profile-461076_960_720.jpg",
+        ]),
   ];
 
   List<TwistMessage> get messageItem => _messageItem;
