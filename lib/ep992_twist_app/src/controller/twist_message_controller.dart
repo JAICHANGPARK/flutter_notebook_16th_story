15392592c1ba1f8251f0e2b5b246b174e6008cf2
Dreamwalker <aristojeff@gmail.com>
2021-12-07 19:53:21 +0900

    Update twist_message_controller.dart

--- lib/ep992_twist_app/src/controller/twist_message_controller.dart
+++ lib/ep992_twist_app/src/controller/twist_message_controller.dart
@@ -5,7 +5,8 @@ class TwistMessageController extends GetxController{
   final List<TwistMessage> _messageItem = [
     TwistMessage(
       name: "Generalist",
-      subtitle: "John: OK! That's super nice. Let's more flutter development"
+      subtitle: "John: OK! That's super nice. Let's more flutter development",
+      dateTime: DateTime.now().subtract(Duration())
     ),
   ];
 
