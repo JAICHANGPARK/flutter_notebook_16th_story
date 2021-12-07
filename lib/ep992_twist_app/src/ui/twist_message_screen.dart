a5cc1fd3f6d1ab561eb2dd4a7f840c2ea19b42a0
Dreamwalker <aristojeff@gmail.com>
2021-12-07 20:06:32 +0900

    Update twist_message_screen.dart

--- lib/ep992_twist_app/src/ui/twist_message_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_message_screen.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_16th_story/ep992_twist_app/src/controller/twist_message_controller.dart';
 import 'package:get/get.dart';
 import 'package:timeago/timeago.dart' as timeago;
+
 class TwistMessageScreen extends StatelessWidget {
   const TwistMessageScreen({Key? key}) : super(key: key);
 
@@ -34,40 +36,85 @@ class TwistMessageScreen extends StatelessWidget {
                 return ListView.builder(
                     itemCount: controller.messageItem.length,
                     itemBuilder: (context, index) {
+                      var messageBlock = controller.messageItem[index];
 
-                      var messageBlock  = controller.messageItem[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 64,
-                          width: 64,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Row(
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 52,
+                              width: 52,
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                              ),
+                              child: messageBlock.images.length > 1
+                                  ? Stack(
+                                      children: [
+                                        Positioned(
+                                          child: CircleAvatar(
+                                            radius: 16,
+                                            backgroundImage: CachedNetworkImageProvider(
+                                              messageBlock.images[0],
+                                            ),
+                                          ),
+                                          top: 0,
+                                          left: 0,
+                                        ),
+                                        Positioned(
+                                          child: CircleAvatar(
+                                            radius: 16,
+                                            backgroundImage: CachedNetworkImageProvider(
+                                              messageBlock.images[1],
+                                            ),
+                                          ),
+                                          bottom: 0,
+                                          right: 0,
+                                        )
+                                      ],
+                                    )
+                                  : Center(
+                                    child: CircleAvatar(
+                                radius: 18,
+                                        backgroundColor: Colors.red,
+                                        backgroundImage: CachedNetworkImageProvider(
+                                          messageBlock.images.first,
+                                        ),
+                                      ),
+                                  ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
                                   children: [
-                                    Text(messageBlock.name ?? "-"),
-                                    Text(timeago.format(messageBlock.dateTime ?? DateTime.now()))
-
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          messageBlock.name ?? "-",
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(timeago.format(messageBlock.dateTime ?? DateTime.now()))
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      height: 4,
+                                    ),
+                                    Text(
+                                      messageBlock.subtitle ?? "-",
+                                      style: const TextStyle(color: Colors.grey, fontSize: 13),
+                                    )
                                   ],
                                 ),
-                                Text(messageBlock.subtitle ?? "-")
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                });
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    });
               },
             ),
           )
