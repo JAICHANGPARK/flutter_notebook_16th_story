d0cd01ca65f1ac67e1a7979c4888d9382bf116f3
Dreamwalker <aristojeff@gmail.com>
2021-12-08 20:05:19 +0900

    Update twist_chat_screen.dart

--- lib/ep992_twist_app/src/ui/twist_chat_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_chat_screen.dart
@@ -8,39 +8,44 @@ class TwistChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.search),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.more_vert),
-                ),
-              ],
-            ),
-            Text("A warm welcome, team"),
-            Row(
-              children: [
-                Text(
-                  "#",
-                  style: TextStyle(color: Colors.orange),
-                ),
-                Text("Lounge")
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert),
+                  ),
+                ],
+              ),
+              Text("A warm welcome, team", style: TextStyle(
+                fontSize: 20,
+              ),),
+              Row(
+                children: [
+                  Text(
+                    "#",
+                    style: TextStyle(color: Colors.orange),
+                  ),
+                  Text("Lounge")
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
