d17e878594c13e65330237d4ba7ca6b04cad0ea7
Dreamwalker <aristojeff@gmail.com>
2021-12-08 19:59:09 +0900

    Update twist_chat_screen.dart

--- lib/ep992_twist_app/src/ui/twist_chat_screen.dart
+++ lib/ep992_twist_app/src/ui/twist_chat_screen.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+
+class TwistChatScreen extends StatelessWidget {
+  const TwistChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
