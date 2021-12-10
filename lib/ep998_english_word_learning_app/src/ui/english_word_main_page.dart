05036c4f1a5515933f7846fcf7191bda5ea5b4d1
Dreamwalker <aristojeff@gmail.com>
2021-12-10 20:05:42 +0900

    Update english_word_main_page.dart

--- lib/ep998_english_word_learning_app/src/ui/english_word_main_page.dart
+++ lib/ep998_english_word_learning_app/src/ui/english_word_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class EwlMainPage extends StatefulWidget {
   const EwlMainPage({Key? key}) : super(key: key);
 
@@ -17,12 +16,17 @@ class _EwlMainPageState extends State<EwlMainPage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Text("Statistic"),
+        foregroundColor: Colors.black,
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.file_upload_outlined),
+          )
+        ],
       ),
       body: Column(
-        children: [
-
-        ],
+        children: [],
       ),
     );
   }
