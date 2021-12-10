59f95f7a20731b92763ac369a88c77e7a210b98a
Dreamwalker <aristojeff@gmail.com>
2021-12-10 19:59:33 +0900

    Create english_word_main_page.dart

---
+++ lib/ep998_english_word_learning_app/src/ui/english_word_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EwlMainPage extends StatefulWidget {
+  const EwlMainPage({Key? key}) : super(key: key);
+
+  @override
+  _EwlMainPageState createState() => _EwlMainPageState();
+}
+
+class _EwlMainPageState extends State<EwlMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
