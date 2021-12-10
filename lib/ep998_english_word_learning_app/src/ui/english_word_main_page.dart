622a7a53e8d298442543add53a36efae7e2d7e6a
Dreamwalker <aristojeff@gmail.com>
2021-12-10 20:02:03 +0900

    Update english_word_main_page.dart

--- lib/ep998_english_word_learning_app/src/ui/english_word_main_page.dart
+++ lib/ep998_english_word_learning_app/src/ui/english_word_main_page.dart
@@ -13,7 +13,12 @@ class _EwlMainPageState extends State<EwlMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text("Statistic"),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
 
