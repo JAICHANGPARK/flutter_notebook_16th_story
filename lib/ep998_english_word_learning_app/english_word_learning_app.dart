4fabf9cab93367a17cb698507152aecfb0f98e81
Dreamwalker <aristojeff@gmail.com>
2021-12-10 20:00:23 +0900

    Update english_word_learning_app.dart

--- lib/ep998_english_word_learning_app/english_word_learning_app.dart
+++ lib/ep998_english_word_learning_app/english_word_learning_app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_16th_story/ep998_english_word_learning_app/src/ui/english_word_main_page.dart';
 class EnglishWordLearningApp extends StatelessWidget {
   const EnglishWordLearningApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return const MaterialApp(
+      home: EwlMainPage(),
     );
   }
 }
