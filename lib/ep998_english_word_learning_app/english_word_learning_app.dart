7a7dfdd7f9ff5538909b56c44363d95ec30d0241
Dreamwalker <aristojeff@gmail.com>
2021-12-10 20:01:41 +0900

    Update english_word_learning_app.dart

--- lib/ep998_english_word_learning_app/english_word_learning_app.dart
+++ lib/ep998_english_word_learning_app/english_word_learning_app.dart
@@ -7,6 +7,7 @@ class EnglishWordLearningApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: EwlMainPage(),
     );
   }
