40b13bd508bf86cf1fdbc5c6642eac0dbd3d462f
Dreamwalker <aristojeff@gmail.com>
2022-10-13 18:47:21 +0900

    Update mh_chat.dart

--- lib/ep978_mental_health_app/src/model/mh_chat.dart
+++ lib/ep978_mental_health_app/src/model/mh_chat.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'mh_chat.freezed.dart';
 
 @freezed
 class MhChat with _$MhChat {
-  factory MhChat({String? uid, String? profileImg, String? msg, String? time, bool? isSend}) = _MhChat;
+  factory MhChat({
+    String? uid,
+    String? profileImg,
+    String? msg,
+    String? time,
+    bool? isSend,
+  }) = _MhChat;
 }
